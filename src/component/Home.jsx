7858b4e8760e8bd40423ee1ac5201ce5dfef8786
eunji <pih9134@naver.com>
2022-12-16 03:07:01 +0900

    12/15

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,23 +2,17 @@ import React from "react";
 
 export default function Home() {
     
-    const user = {
-        name:'mike',
-        age: 30
+    function hasCola(str){
+        if(str.includes('콜라')){
+            console.log('금칙어가 있습니다.');
+        }
+        else {
+            console.log("통과");
+        }
     }
 
-    const showName= Symbol('show name');
-    user[showName] = function () {
-        console.log(this.name)
-    }
-
-    user[showName]();
-    
-    for(let key in user){
-        console.log(`His ${key} is ${user[key]}.`);
-    }
-
-
+    hasCola('사이다가 좋아');
+    hasCola('콜라가 좋아');
 
     return (
         <div></div>
