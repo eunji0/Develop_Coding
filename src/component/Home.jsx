fdd3ec57fb0c564bda4ba2d5d7f4f8347699f94d
eunji <pih9134@naver.com>
2023-01-31 23:38:12 +0900

    kur 1/31

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,24 +3,6 @@ import React from "react";
 export default function Home() {
     
 
-    const user = {
-        name: 'mike',
-        showName: function (){
-            console.log(`hello, ${this.name}`);
-        },
-    };
-
-    user.showName(); //hello, mike
-
-    let fn =user.showName;
-
-    fn();//hello, 
-
-    fn.call(user);
-
-    let boundfn =fn.bind(user);
-    boundfn();
-;
     return (
         <div></div>
     );
