15648a84019bda46bc57c016f726838eb3618767
eunji <pih9134@naver.com>
2023-03-08 23:51:56 +0900

    dev 3/8

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+$("p").click(function(){
+    $(this).hide();
+});
 
 export default function Home() {
     
 
     return (
-        <div></div>
+        <div>
+            <p>안녕하새요</p>
+        </div>
     );
 }
