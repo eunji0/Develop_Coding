4ddfb8b3d25688a91e34676d6c4968abbdbad910
eunji <pih9134@naver.com>
2023-03-13 23:22:57 +0900

    3/13

--- src/dev/dev5.js
+++ src/dev/dev5.js
@@ -1,1 +1,23 @@
 //올바른 괄호
+//괄호가 올바른게 Paired 되었다는 것은 "(", "{", "[", 등의 괄호가 열렸으면
+//반드시 짝지어서 ")","}","]"가 닫혀야한다는 말
+
+function vp(input){
+    const stack=[]
+    for(let char of input){
+        if(char==="[" || char==="{"||char==="("){
+            stack.push(char)
+        }else{
+            const last=stack.pop()
+            if(last === "{" && char!=="}"||
+            last === "[" && char !== "]" ||
+            last === "(" && char !==")"){
+                return false
+            }
+        }
+    }
+    return stack.length===0;
+}
+
+console.log(vp("[]{{"))
+console.log(vp("{[]}"))
