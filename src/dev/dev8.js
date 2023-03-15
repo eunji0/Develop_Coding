4b0617369e88ff7dcb015600516290c786cdaced
eunji <pih9134@naver.com>
2023-03-16 00:01:48 +0900

    3/16

---
+++ src/dev/dev8.js
@@ -0,0 +1,20 @@
+const obj1={
+    a:"a",
+    b:1,
+    c:["a","b","c"],
+    d:{
+        e:null,
+        f:-1
+    }
+}
+
+const obj2 = {
+    a:"a",
+    b:1,
+    c:["a","b","c"],
+    d:{
+        e:null,
+        f:-1
+    }
+}
+
