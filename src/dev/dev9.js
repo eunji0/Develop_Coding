8c393a3c463f3339ce336f479589da00cb71b31e
eunji <pih9134@naver.com>
2023-03-19 02:03:39 +0900

    3/19

--- src/dev/dev9.js
+++ src/dev/dev9.js
@@ -1,1 +1,24 @@
 //순열
+
+function per(arr){
+    const result=[];
+
+    const dfs=(i,arr)=>{
+        if(i===arr.length){
+            return result.push([...arr])
+        }
+
+        for(let j=1;j<arr.length;j++){
+            //swap
+            [arr[i], arr[j]]=[arr[j], arr[i]]
+            //dfs
+            dfs(i+1, arr);
+            //swap back
+            [arr[i], arr[j]]=[arr[j], arr[i]]
+        }
+    }
+    dfs(0, arr);
+    return result
+}
+
+console.log(per(["a","b","c"]))
