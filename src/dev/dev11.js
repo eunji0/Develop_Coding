57d16e461ea08e1c478798b806b1d0c2c6fdd877
eunji <pih9134@naver.com>
2023-03-21 23:26:12 +0900

    3/21

--- src/dev/dev11.js
+++ src/dev/dev11.js
@@ -1,2 +1,22 @@
 //배열 0 옮기기
 //(move seros)
+
+const input=[0,1,0,3,12]
+
+function movezero(nums){
+    let j=0;
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==0){
+            //swap
+            let temp=nums[j]
+            nums[j]=nums[i]
+            nums[i]=temp
+
+            j++
+        }
+    }
+    return nums
+}
+
+console.log(movezero(input));
