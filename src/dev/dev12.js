6e01594df6be96fab4de1f4cff5234752e75151e
eunji <pih9134@naver.com>
2023-03-27 00:45:01 +0900

    3/27

--- src/dev/dev12.js
+++ src/dev/dev12.js
@@ -7,4 +7,27 @@ function inte(intervals){
     //조건문
     //1.이전 구간의 끝시간 >= 그 다음 구간의 시작시간 -> 병합
     //2. 아닐 경우 병합 x -> 결과리스트에 추가
+
+    if(intervals.length===0){
+        return []
+    }else if(intervals.length===1){
+        return intervals
+    }
+
+    //구간 시작시간 기준으로 정렬
+    intervals.sort((a,b)=>a[0]-b[0])
+    let result=[intervals[0]]
+
+    for(let interval of intervals){
+        let previous=result[result.length-1]
+        if(previous[1]>=interval[0]){
+            //merge
+            previous[1]=Math.max(previous[1], interval[1])
+        }else{
+            result.push(interval)
+        }
+    }
+    return result
 }
+
+console.log(inte(input));
