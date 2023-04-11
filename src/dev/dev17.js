b2009725468a61785481ee06a96df08bfe129e67
eunji <pih9134@naver.com>
2023-04-11 23:51:56 +0900

    4/11

--- src/dev/dev17.js
+++ src/dev/dev17.js
@@ -1,1 +1,35 @@
 //회문순열
+//주어진 문자열이 회문의 순열인지 아닌ㄴ지 확인하는 함수를 작성하라.
+//회문이란 앞으로 읽으나 뒤로 읽으나 같은 단어 혹은 구절을 의미하며,
+//순열이란 문자열을 재배치하는 것을 뜻한다. 회문이 꼭 사전에 등장하는 단어로 제한될 필요는 없다.
+//(대소문자와 빈글자는 무시한다고 가정)
+
+function cmap(s){
+    let map = new Map();
+    let charCount = 0;
+    s = s.toLowerCase();
+
+    for(let char of s){
+        if(char === " "){
+            continue;
+        }
+
+        if(map.get(char)){
+            map.delete(char);
+        }else{
+            map.set(char, true);
+        }
+        charCount++;
+    }
+
+    if(charCount %2 ===0){
+        return map.size === 0;
+    }else{
+        return map.size === 1;
+    }
+
+
+}
+
+console.log(cmap("Tact Coa"));
+console.log(cmap("Tact Boa"));
