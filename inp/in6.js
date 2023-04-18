5a57718de319946d5808d82a661c0e17b763feda
eunji <pih9134@naver.com>
2023-04-18 13:43:59 +0900

    4/18

--- inp/in6.js
+++ inp/in6.js
@@ -1,14 +1,23 @@
 // 변수를 선언합니다.
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var array=[-3, 12]
 // 메서드를 사용합니다.
 array = array.every(function (element, index, array) {
-return element <= 5;
+if(element<0){
+    return false
+}else{
+    return true
+}
 });
 // 출력합니다.
 console.log(array);
 
-const isBelowThreshold = (currentValue) => currentValue < 40;
+const isBelowThreshold = (currentValue) => currentValue < 0;
 
 const array1 = [1, 30, 39, 29, 10, 13];
 
 console.log(array1.every(isBelowThreshold));
+
+const isTopThreshold = (currentValue) => currentValue > 0;
+
+console.log(array1.every(isTopThreshold));
