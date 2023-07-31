becab9f5daef5b1866247d66b28780fbd4cec414
unknown <pih9134@naver.com>
2023-08-01 01:52:07 +0900

    dev: 안전지대

--- src/dev/dev24.js
+++ src/dev/dev24.js
@@ -3,47 +3,47 @@
 function solution(s) {
   s = s.split(' ')
   let result = []
-  for(let i of s){
-      if(i === 'Z'){
-          result.pop()
-      }else{
-          result.push(+i)
-      }
+  for (let i of s) {
+    if (i === 'Z') {
+      result.pop()
+    } else {
+      result.push(+i)
+    }
   }
-  return result.reduce((a, c) => a+c, 0);
+  return result.reduce((a, c) => a + c, 0);
 }
 
 //등수 매기기
 function solution(score) {
-  let sum= score.map((v)=>v[0]+v[1])
-  let down = sum.slice().sort((a, b) => b-a);
-   return sum.map((v)=>down.indexOf(v)+1)
+  let sum = score.map((v) => v[0] + v[1])
+  let down = sum.slice().sort((a, b) => b - a);
+  return sum.map((v) => down.indexOf(v) + 1)
 }
 
 //저주의 숫자 3
 function solution(n) {
-  let answer = 0 ;
-  for(let i = 0; i<n; i++){
-      answer+=1
-      while(answer%3===0 || answer.toString().split('').includes('3')){
-          answer+=1
-      }
+  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    answer += 1
+    while (answer % 3 === 0 || answer.toString().split('').includes('3')) {
+      answer += 1
+    }
   }
   return answer
 }
 
 //다항식 더하기
-function solution(p){
+function solution(p) {
   let a = p.split(' + ')
-  let answer= [];
-  let x = a.filter((v)=>v.includes('x')).map((v)=>parseInt(v.replace('x', ''))||1).reduce((a,c)=>a+c,0)
-  let b = a.filter((v)=>!v.includes('x')).reduce((a,c)=>a+parseInt(c),0)
-  if(x === 1) {
+  let answer = [];
+  let x = a.filter((v) => v.includes('x')).map((v) => parseInt(v.replace('x', '')) || 1).reduce((a, c) => a + c, 0)
+  let b = a.filter((v) => !v.includes('x')).reduce((a, c) => a + parseInt(c), 0)
+  if (x === 1) {
     answer.push('x')
-  }else{
+  } else {
     answer.push(`${x}x`)
   }
-  if(b){
+  if (b) {
     answer.push(b)
   }
   return answer.join(' + ')
@@ -75,3 +75,36 @@ console.log(uniqueSet2.size); // 3
 //Set을 배열로 변환하기
 const arrayFromSet = Array.from(uniqueSet2); // [1, 2, 3]
 const arrayFromSet2 = [...uniqueSet2]; // [1, 2, 3]
+
+
+//안전지대
+function solution(board) {
+  let n = board.length
+  const d = [
+    [0, 0],
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [-1, -1],
+    [-1, 0],
+    [-1, 1]
+  ];
+  let danger = new Set();
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        d.forEach(v => {
+          let [col, row] = [i + v[0], j + v[1]];
+          if (col >= 0 && col < n && row >= 0 && row < n) {
+            danger.add(col + " " + row);
+          }
+        });
+      }
+    }
+  }
+  return n * n - danger.size;
+}
+
+solution([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]])
