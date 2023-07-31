40c914c55893fe8a2370ebe84a4955eea989e0a5
unknown <pih9134@naver.com>
2023-07-31 17:46:17 +0900

    dev: 영어가 싫어요

--- src/dev/dev23.js
+++ src/dev/dev23.js
@@ -44,3 +44,8 @@ function solution(emergency) {
   let dm = emergency.slice().sort((a,b)=>b-a)
   return emergency.map(v=>dm.indexOf(v)+1) 
 }
+
+//영어가 싫어요
+function solution(numbers) {
+  return +numbers.replaceAll('one', 1).replaceAll('two', 2).replaceAll('three', 3).replaceAll('four', 4).replaceAll('five', 5).replaceAll('six', 6).replaceAll('seven', 7).replaceAll('eight', 8).replaceAll('nine', 9).replaceAll('zero', 0)
+}
