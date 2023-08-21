9a4297c1838a2920e29480656dfc77dfeff7ee49
unknown <pih9134@naver.com>
2023-08-21 18:39:15 +0900

    Re: 가까운 수

--- src/dev/dev28.js
+++ src/dev/dev28.js
@@ -85,4 +85,19 @@ function solution(i, j, k){
 
 solution(1, 13, 1)
 
-//17, 19, 20
+//가까운 수
+function solution(array, n){
+  array.sort((a, b) => a-b)
+  let s =0;
+  for(let i=0; i<array.length; i++){
+    if(Math.abs(array[i]-n<s)){
+      s=array[i]
+    }
+  }
+
+  return s
+}
+
+solution([3,10,28], 20)
+
+//17, 19, 21, 22
