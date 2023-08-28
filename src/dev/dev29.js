141c970b75c1cba2f2562c6773d0b127c70288df
unknown <pih9134@naver.com>
2023-08-28 18:02:49 +0900

    숫자 문자열과 영단어

--- src/dev/dev29.js
+++ src/dev/dev29.js
@@ -263,3 +263,16 @@ function solution(sizes) {
     }
     return a*b
 }
+
+//숫자 문자열과 영단어ㅏ
+function solution(s) {
+  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  var answer = s;
+
+  for(let i=0; i< numbers.length; i++) {
+      let arr = answer.split(numbers[i]);
+      answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
