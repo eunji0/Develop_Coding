5e49f3488f77c463bcce56a59191b779f6cbf25d
unknown <pih9134@naver.com>
2023-09-01 23:10:23 +0900

    문자열 내 마음대로 정렬하기

--- src/dev/dev29.js
+++ src/dev/dev29.js
@@ -330,3 +330,13 @@ function decodeRow(row1, row2) {
     return decodedRow;
 }
 
+//문자열 내 마음대로 정렬하기
+function solution(strings, n) {
+    return strings.sort((a, b)=>{
+        if(a[n]===b[n]){
+            return a.localeCompare(b)
+        }else{
+            return a[n].localeCompare(b[n])
+        }
+    })
+}
