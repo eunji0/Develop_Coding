682929e46b7d68aa8bb45beb45694f9100135740
unknown <pih9134@naver.com>
2023-09-01 23:49:32 +0900

    두개 뽑아서 더하기

--- src/dev/dev29.js
+++ src/dev/dev29.js
@@ -357,3 +357,18 @@ function solution(s) {
 
     return result;
 }
+
+//두개 뽑아서 더하기
+function solution(numbers) {
+    const temp = []
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            temp.push(numbers[i] + numbers[j])
+        }
+    }
+
+    const answer = [...new Set(temp)]
+
+    return answer.sort((a, b) => a - b)
+}
