0205ab9e38e25d13c7225f45a058aaf1dfe86441
unknown <pih9134@naver.com>
2023-09-03 01:56:12 +0900

    콜라 문제

--- src/dev/dev29.js
+++ src/dev/dev29.js
@@ -397,3 +397,13 @@ function solution(food){
 
   return answer+'0'+[...answer].reverse().join('')
 }
+
+//콜라 문제
+function solution(a, b, n) {
+    let change =0 
+    while(n>=a){
+        change+=Math.floor(n/a)*b
+        n=Math.floor(n/a)*b + n%a
+    }
+    return change
+}
