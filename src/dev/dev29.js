e0f1066d23809587cfa5c47b4713940b68d5043b
unknown <pih9134@naver.com>
2023-09-03 21:02:11 +0900

    카드 뭉치

--- src/dev/dev29.js
+++ src/dev/dev29.js
@@ -461,3 +461,19 @@ function solution(a, b){
   const d2 = d1.getDay()
   return days[d2]
 }
+
+//카드 뭉치
+function solution(cards1, cards2, goal){
+  for(const s of goal){
+    if(cards1[0]===s){
+      cards1.shift()
+    }else if(cards2[0]===s){
+      cards2.shift()
+    }
+    else{
+      return "No"
+    }
+  }
+
+  return "Yes"
+}
