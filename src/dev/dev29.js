2470267e8cd71a8e7c0110626373eddae358616d
unknown <pih9134@naver.com>
2023-09-08 00:33:02 +0900

    완주라지 못한 선수

--- src/dev/dev29.js
+++ src/dev/dev29.js
@@ -664,3 +664,15 @@ function solution(X, Y) {
   
   return answer.split('').sort((a, b)=>Number(b)-Number(a)).join('')
 }
+
+//완주하지 못한 선수
+function solution(participant, completion) {
+    participant.sort()
+    completion.sort()
+    for(let i=0; i<participant.length; i++){
+        if(participant[i]!=completion[i]){
+            return participant[i]
+        }
+    }
+}
+
