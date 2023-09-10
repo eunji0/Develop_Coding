78b258b709c6f99d9df41c88bdd75a7e31505361
unknown <pih9134@naver.com>
2023-09-11 00:14:32 +0900

    키패드 누르기

--- src/dev/dev29.js
+++ src/dev/dev29.js
@@ -789,3 +789,99 @@ function solution(n, lost, reserve) {
   }
   return a.filter((v)=>v>0).length
 }
+
+//크레인 인형뽑기 게임
+function solution(board, moves) {
+  let answer = 0;
+  let stack = [];
+
+  moves.forEach((move) => {
+    for (let i = 0; i < board.length; i++) {
+      const item = board[i][move - 1]
+
+      if (item === 0) continue
+
+      if (item === stack[stack.length - 1]) {
+        stack.pop()
+        answer += 2
+      } else {
+        stack.push(item)
+      }
+
+      board[i][move - 1] = 0
+      break;
+    }
+  })
+
+  return answer
+}
+
+//키패드 누르기
+function solution(numbers, hand) {
+  let answer = [];
+  let leftHandPosition = "*";
+  let rightHandPosition = "#";
+
+  numbers.forEach((num) => {
+    if (num === 1 || num === 4 || num === 7) {
+      answer.push('L')
+      leftHandPosition = num
+      return
+    }
+    if (num === 3 || num === 6 || num === 9) {
+      answer.push('R')
+      rightHandPosition = num
+      return
+    }
+
+    const leftHandDistance = getDistance(leftHandPosition, num);
+    const rightHandDistance = getDistance(rightHandPosition, num);
+
+    if (leftHandDistance === rightHandDistance) {
+      if (hand == 'right') {
+        answer.push('R')
+        rightHandPosition = num
+        return
+      } else {
+        answer.push('L')
+        leftHandPosition = num
+        return
+      }
+    }
+
+    if (leftHandDistance > rightHandDistance) {
+      answer.push('R')
+      rightHandPosition = num
+    } else {
+      answer.push("L");
+      leftHandPosition = num;
+    }
+  })
+  return answer.join("");
+
+}
+
+const getDistance = (locatedNumber, target) => {
+  const keyPad = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2],
+    "*": [3, 0],
+    0: [3, 1],
+    "#": [3, 2],
+  };
+
+  const nowPosition = keyPad[locatedNumber];
+  const targetPosition = keyPad[target];
+
+  return (
+    Math.abs(targetPosition[0] - nowPosition[0]) +
+    Math.abs(targetPosition[1] - nowPosition[1])
+  );
+};
