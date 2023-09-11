39bafa2b275e92a863167e913b3b01050ccbade6
unknown <pih9134@naver.com>
2023-09-12 02:14:35 +0900

    x만큼 간격이 있는 n개의 숫자

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -34,3 +34,12 @@ function solution(n) {
   return sum
 }
 
+//x만큼 간격이 있는 n개의 숫자
+function solution(x, n) {
+  var answer = [];
+  for(let i=1; i<=n; i++){
+      answer.push(x*i)
+  }
+  return answer
+}
+
