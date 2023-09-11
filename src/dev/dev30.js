c9e6d259b90996f951d4a061d5abea0fd8199ad8
unknown <pih9134@naver.com>
2023-09-12 01:46:36 +0900

    나머지가 1이 되는 수 찾기

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -3,3 +3,12 @@ function solution(n)
 {
     return n.toString().split('').reduce((a, c)=>a+Number(c), 0)
 }
+
+//나머지가 1이 되는 수 찾기
+function solution(n) {
+  for(let i=0; i<n; i++){
+      if(n%i===1){
+          return i
+      }
+  }
+}
