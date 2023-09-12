40f944fca4257b334618406e7332467f9a079b52
unknown <pih9134@naver.com>
2023-09-13 02:02:56 +0900

    하샤드수

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -71,3 +71,13 @@ function solution(n) {
 function solution(n) {
     return +(n.toString().split('').sort((a, c)=>c-a).join(''))
 }
+
+//하샤드 수
+function solution(x) {
+     let a = x.toString().split('')
+     let sum=0;
+     for(let i=0; i<a.length; i++){
+         sum+=+a[i]
+     }
+    return x%sum===0 ? true:false
+}
