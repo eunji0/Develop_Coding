f25e3a17d2cb00dfe3068b3025c06c7700dfaf73
unknown <pih9134@naver.com>
2023-09-17 00:15:53 +0900

    숫자 문자열과 영단어

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -321,3 +321,26 @@ function solution(s, n) {
     var chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY                          "
     return s.split('').map(v=>chars[chars.indexOf(v)+n]).join('')
 }
+
+//숫자 문자열과 영단어
+function solution(s) {
+    const words = {
+       "zero": 0,
+       "one": 1,
+       "two": 2,
+       "three": 3,
+       "four": 4,
+       "five": 5,
+       "six": 6,
+       "seven": 7,
+       "eight": 8,
+       "nine": 9,
+   };
+   
+   for(const word in words){
+       let r =RegExp(word, 'g')
+       s=s.replace(r, words[word])
+   }
+   
+   return +s
+}
