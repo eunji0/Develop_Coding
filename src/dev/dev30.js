f5521ee8492c9e6b8c9cbcdbaf3305894f7f4e7e
unknown <pih9134@naver.com>
2023-09-18 00:01:57 +0900

    k번째 수

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -369,3 +369,13 @@ function solution(n, arr1, arr2) {
 function solution(strings, n) {
     return strings.sort((a, b)=>a[n]===b[n]? a.localeCompare(b) : a[n].localeCompare(b[n]))
 }
+
+//k번째 수
+function solution(array, commands) {
+    var answer = [];
+    for(let i=0; i<commands.length; i++){
+        let a = array.slice(commands[i][0]-1, commands[i][1]).sort((a, b)=>a-b)
+        answer.push(a[commands[i][2]-1])
+    }
+    return answer;
+}
