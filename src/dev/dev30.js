1f4f16681675f45b6a7f20caabee77c4bac0d294
unknown <pih9134@naver.com>
2023-09-20 02:06:06 +0900

    카드 뭉치

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -459,3 +459,18 @@ function solution(k, score) {
     }
     return a;
 }
+
+//카드 뭉치
+function solution(cards1, cards2, goal) {
+    for(let i of goal){
+        if(cards1[0]===i){
+            cards1.shift()
+        }else if(cards2[0]===i){
+            cards2.shift()
+        }
+        else{
+            return 'No'
+        }
+    }
+    return 'Yes'
+}
