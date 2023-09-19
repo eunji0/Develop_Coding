6758c3f911807d111d381c235844bee0a2ae07b8
unknown <pih9134@naver.com>
2023-09-19 21:57:03 +0900

    두 개 뽑아서 더하기

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -396,3 +396,17 @@ function solution(s) {
     }
     return answer
 }
+
+//두 개 뽑아서 더하기
+function solution(numbers) {
+    let a = []
+    for(let i=0; i<numbers.length; i++){
+        for(let j=i+1; j<numbers.length; j++){
+            a.push(numbers[i]+numbers[j])
+        }
+    }
+
+    const answer = [...new Set(a)]
+
+    return answer.sort((a, b)=>a-b)
+}
