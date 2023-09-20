2c79be5056070332dcf05dcbf3fb6f40c0fbb408
unknown <pih9134@naver.com>
2023-09-20 22:18:51 +0900

    소수 만들기

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -531,3 +531,29 @@ function solution(answers) {
     }
     return result
 }
+
+//소수 만들기
+function isPrime(num) {
+    if(num<2) return false
+    for(let i=2; i*i<=num;i++){
+        if(num%i===0) return false
+    }
+    return true
+}
+
+function solution(nums){
+    let c=0
+    
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            for(let k=j+1; k<nums.length; k++){
+                let sum =nums[i]+nums[j]+nums[k]
+                
+                if(isPrime(sum)){
+                    c++
+                }
+            }
+        }
+    }
+    return c
+}
