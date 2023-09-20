198cf9c467d9dccc40b20b3716b98677db37055f
unknown <pih9134@naver.com>
2023-09-20 19:32:18 +0900

    모의고사

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -501,3 +501,33 @@ function solution(k, m, score) {
     }
     return answer
 }
+
+//모의고사
+function solution(answers) {
+    const patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ];
+    
+    const score=[0,0,0]
+    const result=[]
+    
+    for(let i=0; i<answers.length; i++){
+        for(let j=0; j<3; j++){
+            const pattern = patterns[j]
+            if(answers[i]===pattern[i%pattern.length]){
+                score[j]++
+            }
+        }
+    }
+    
+    const maxScore=Math.max(...score)
+    
+    for(let i=0; i<score.length; i++){
+        if(score[i]===maxScore){
+            result.push(i+1)
+        }
+    }
+    return result
+}
