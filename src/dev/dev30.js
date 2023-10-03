9cb5cdbbc5116593c2ffce94ad65edd74503c761
unknown <pih9134@naver.com>
2023-10-04 00:38:09 +0900

    크레인 인형뽑기 게임

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -803,3 +803,30 @@ function solution(s, skip, index) {
     }
     return result
 }
+
+//크레인 인형뽑기 게임
+function solution(board, moves) {
+    let answer=0;
+    let stack=[]
+
+    moves.forEach((move)=>{
+        for(let i=0; i<board.length; i++){
+            let item = board[i][move-1]
+
+            if(item===0) continue;
+
+            if(item===stack[stack.length-1]){
+                stack.pop()
+                answer+=2
+            }else{
+                stack.push(item)
+            }
+
+            board[i][move-1]=0
+
+            break;
+        }
+    })
+
+    return answer
+}
