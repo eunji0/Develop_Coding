9f9b57590a1985913d82a35eaf1dc49b3a4ee2ec
unknown <pih9134@naver.com>
2023-10-16 21:22:57 +0900

    숫자의 합만들기

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -1153,3 +1153,13 @@ function solution(s){
     }
     return cnt === 0 ? true : false
 }
+
+//숫자의 합만들기
+function solution(n) {
+    var answer = 0;
+    for (let i  =0; i <=n; i++) {
+        if ( n % i === 0 && i % 2 === 1 )   //약수이면서 홀수인 경우 
+            answer ++
+    }
+    return answer;
+}
