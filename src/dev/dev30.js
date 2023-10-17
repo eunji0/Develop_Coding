9aa87894cc3d4837b236c32dacc0a26047fdc21f
unknown <pih9134@naver.com>
2023-10-18 02:53:47 +0900

    이진 변환 반복하기

--- src/dev/dev30.js
+++ src/dev/dev30.js
@@ -1163,3 +1163,19 @@ function solution(n) {
     }
     return answer;
 }
+
+//이진 변환 반복하기
+function solution(s) {
+    let answer = [0, 0];
+    let sLen = 0;
+  
+    while (s.length > 1) {
+      sLen = s.length;
+      s = s.split("0").join("");
+      answer[0]++;
+      answer[1] += (sLen - s.length);
+      s = s.length.toString(2);
+    }
+  
+    return answer;
+  }
