a2f2d312c85403e89e9a98329c7fe2aa6bb8be43
unknown <pih9134@naver.com>
2023-11-18 03:37:31 +0900

    피보나치

---
+++ src/groom/groom.js
@@ -0,0 +1,35 @@
+//팩토리얼
+
+// Run by Node.js
+const readline = require("readline");
+
+(async () => {
+  let rl = readline.createInterface({ input: process.stdin });
+
+  let n = 0;
+  for await (const line of rl) {
+    n = +line;
+  }
+
+  rl.close();
+
+  function factorial(n) {
+    let result = BigInt(1);
+    for (let i = 2n; i <= BigInt(n); i++) {
+      result *= i;
+    }
+    return result;
+  }
+
+  let total = factorial(n);
+
+  while (total >= 10n) {
+    total = total
+      .toString()
+      .split("")
+      .reduce((a, c) => a + BigInt(c), BigInt(0));
+  }
+  console.log(total.toString());
+
+  process.exit();
+})();
