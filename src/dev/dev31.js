48d518585a8d1370c398f41ab2f87b9c3dc92449
unknown <pih9134@naver.com>
2023-12-22 03:21:06 +0900

    귤고르기

--- src/dev/dev31.js
+++ src/dev/dev31.js
@@ -234,3 +234,23 @@ function solution(n, words) {
 
   return answer;
 }
+
+//귤고르기
+function solution(k, tangerine) {
+  const obj = {}
+  tangerine.forEach(n=>{
+      obj[n]=++obj[n]||1
+  })
+  
+  const kinds = Object.values(obj).sort((a, b)=>b-a);
+  let answer=0;
+  let sum=0;
+  
+  for(let num of kinds){
+      ++answer;
+      sum+=num;
+      
+      if(sum>=k) break;
+  }
+  return answer;
+}
