2ff49123b7a73e837200950204257cd70e7d177d
unknown <pih9134@naver.com>
2023-12-29 03:14:49 +0900

    네트워크

--- src/dev/dev31.js
+++ src/dev/dev31.js
@@ -478,3 +478,29 @@ function solution(numbers, target) {
 
   return answer;
 }
+
+//네트워크
+function solution(n, computers) {
+  let answer = 0;
+  const length = computers.length;
+  const visited = Array.from({ length: n }, () => false);
+
+  function dfs(index) {
+    visited[index] = true;
+
+    for (let i = 0; i < length; i++) {
+      if (computers[index][i] && !visited[i]) {
+        dfs(i);
+      }
+    }
+  }
+
+  for (let i = 0; i < length; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      answer++;
+    }
+  }
+
+  return answer;
+}
