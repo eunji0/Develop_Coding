d67dbbb75a80091ff68a910bc0802c0ae31a715a
unknown <pih9134@naver.com>
2023-12-29 00:44:06 +0900

    타켓넘버

--- src/dev/dev31.js
+++ src/dev/dev31.js
@@ -455,3 +455,26 @@ function solution(begin, target, words) {
 
   return answer;
 }
+
+//타겟넘버
+function solution(numbers, target) {
+  let answer = 0;
+  const length = numbers.length;
+
+  function dfs(count, sum) {
+    if (count === length) {
+      if (target === sum) {
+        answer++;
+      }
+      return;
+    }
+
+    console.log(count);
+    dfs(count + 1, sum + numbers[count]);
+    dfs(count + 1, sum - numbers[count]);
+  }
+
+  dfs(0, 0);
+
+  return answer;
+}
