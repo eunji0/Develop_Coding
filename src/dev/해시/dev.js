2cf57a578d4f70c0efca05ee52520e998c35e9e8
unknown <pih9134@naver.com>
2024-01-12 00:42:06 +0900

    전화번호 목록

--- src/dev/해시/dev.js
+++ src/dev/해시/dev.js
@@ -30,3 +30,15 @@ function solution2(nums) {
   let b = Array.from(new Set(nums)).length;
   return a < b ? a : b;
 }
+
+//전화번호 목록
+function solution3(phone_book) {
+  phone_book.sort();
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
+      return false;
+    }
+    return true;
+  }
+}
