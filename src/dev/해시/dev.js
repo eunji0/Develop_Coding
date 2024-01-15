5314403816b0b216a6dff6bee24eef95a7366688
unknown <pih9134@naver.com>
2024-01-16 01:36:16 +0900

    달리기 경주

--- src/dev/해시/dev.js
+++ src/dev/해시/dev.js
@@ -96,3 +96,28 @@ function solution5(genres, plays) {
     return acc; // 결과 배열 반환
   }, []);
 }
+
+//달리기 경주
+function solution6(players, callings) {
+  const playerMap = {};
+
+  // calling 원소들의 idx를 미리 구해서 저장
+  for (let i = 0; i < players.length; i++) {
+    playerMap[players[i]] = i;
+  }
+
+  for (let i = 0; i < callings.length; i++) {
+    const idx = playerMap[callings[i]];
+    const temp = players[idx - 1];
+
+    // 해당 idx와 이전 idx의 원소를 swap
+    players[idx - 1] = callings[i];
+    players[idx] = temp;
+
+    // map의 idx도 갱신
+    playerMap[callings[i]] = idx - 1;
+    playerMap[temp] = idx;
+  }
+
+  return players;
+}
