b13b314b70790caf1f4d7d79882c07a1ebdd975c
unknown <pih9134@naver.com>
2024-01-17 00:53:58 +0900

    추억 점수

--- src/dev/해시/dev.js
+++ src/dev/해시/dev.js
@@ -121,3 +121,15 @@ function solution6(players, callings) {
 
   return players;
 }
+
+//추억 점수
+function solution7(name, yearning, photo) {
+  let obj = {};
+  for (let i = 0; i < name.length; i++) {
+    obj[name[i]] = yearning[i];
+  }
+
+  return photo.map((value) =>
+    value.map((v) => (obj[v] ? obj[v] : 0)).reduce((a, c) => a + c, 0)
+  );
+}
