681164762ae9da086fdfddb28dd47bf8fe75be68
unknown <pih9134@naver.com>
2024-01-18 01:00:16 +0900

    대충 만든 자판

--- src/dev/해시/dev.js
+++ src/dev/해시/dev.js
@@ -208,3 +208,26 @@ function solution(n, m, section) {
   }
   return answer;
 }
+
+//대충 만든 자판
+function solution(keymap, targets) {
+  let answer = [];
+  let map = new Map();
+
+  for (let key of keymap) {
+    for (let i = 0; i < key.length; i++) {
+      if (!map.has(key[i]) || i + 1 < map.get(key[i])) map.set(key[i], i + 1);
+    }
+  }
+
+  for (let target of targets) {
+    let c = 0;
+    for (let i = 0; i < target.length; i++) {
+      c += map.get(target[i]);
+    }
+
+    answer.push(c || -1);
+  }
+
+  return answer;
+}
