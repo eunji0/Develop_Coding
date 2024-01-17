1fbc41b61cc7c9826b920c5513f78eaa235171fd
unknown <pih9134@naver.com>
2024-01-18 00:18:40 +0900

    바탕화면 정리

--- src/dev/해시/dev.js
+++ src/dev/해시/dev.js
@@ -175,3 +175,23 @@ function solution(park, routes) {
 
   return [setR, setC];
 }
+
+//바탕화면 정리
+function solution(wallpaper) {
+  let x = [];
+  let y = [];
+
+  for (let i = 0; i < wallpaper.length; i++) {
+    for (let j = 0; j < wallpaper[i].length; j++) {
+      if (wallpaper[i][j] === "#") {
+        y.push(i);
+        x.push(j);
+      }
+    }
+  }
+
+  x.sort((a, b) => a - b);
+  y.sort((a, b) => a - b);
+
+  return [y[0], x[0], y[y.length - 1] + 1, x[x.length - 1] + 1];
+}
