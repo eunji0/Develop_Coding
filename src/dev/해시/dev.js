ec51fe51e86cea78c4db4e0710c21938ddf00380
unknown <pih9134@naver.com>
2024-01-22 20:46:26 +0900

    신고 결과 받기

--- src/dev/해시/dev.js
+++ src/dev/해시/dev.js
@@ -525,3 +525,30 @@ function solution(survey, choices) {
   console.log(MBTI);
   return types.map(([a, b]) => (MBTI[b] > MBTI[a] ? b : a)).join("");
 }
+
+//신고 결과 받기
+function solution(id_list, report, k) {
+  var answer = new Array(id_list.length).fill(0);
+  var report_list = {};
+
+  id_list.map((user) => {
+    report_list[user] = [];
+  });
+
+  report.map((user) => {
+    const [user_id, report_id] = user.split(" ");
+    if (!report_list[report_id].includes(user_id)) {
+      report_list[report_id].push(user_id);
+    }
+  });
+
+  for (const key in report_list) {
+    if (report_list[key].length >= k) {
+      report_list[key].map((user) => {
+        answer[id_list.indexOf(user)] += 1;
+      });
+    }
+  }
+
+  return answer;
+}
