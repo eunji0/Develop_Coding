75f90d18e6c4f93093d582d3e9bf93c0f24d6ebc
unknown <pih9134@naver.com>
2024-01-22 18:40:31 +0900

    콜라문제

--- src/dev/해시/dev.js
+++ src/dev/해시/dev.js
@@ -456,3 +456,13 @@ function solution(babbling) {
     return (possible += 1);
   }, 0);
 }
+
+//콜라 문제
+function solution(a, b, n) {
+  let answer = 0;
+  while (n >= a) {
+    answer += Math.floor(n / a) * b;
+    n = Math.floor(n / a) * b + (n % a);
+  }
+  return answer;
+}
