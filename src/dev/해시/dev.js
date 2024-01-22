8a54c3ee867b04420e34b2f467e10b7ce89f4fde
unknown <pih9134@naver.com>
2024-01-23 01:26:01 +0900

    나머지가 1이 되는 수 찾기

--- src/dev/해시/dev.js
+++ src/dev/해시/dev.js
@@ -552,3 +552,12 @@ function solution(id_list, report, k) {
 
   return answer;
 }
+
+//나머지가 1이 되는 수 찾기
+function solution(n) {
+  for (let i = 0; i < n; i++) {
+    if (n % i === 1) {
+      return i;
+    }
+  }
+}
