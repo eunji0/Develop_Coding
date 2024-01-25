3ab10f7fb42ede71a2e0fa58d15d3cf26d994aae
unknown <pih9134@naver.com>
2024-01-26 00:41:18 +0900

    백준/3003

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -1,7 +1,19 @@
 //25083 새싹
-console.log("         ,r'\"7");
-console.log("r`-_   ,'  ,/");
-console.log(" \\. \". L_r'");
-console.log("   `~\\/");
-console.log("      |");
-console.log("      |");
+// console.log("         ,r'\"7");
+// console.log("r`-_   ,'  ,/");
+// console.log(" \\. \". L_r'");
+// console.log("   `~\\/");
+// console.log("      |");
+// console.log("      |");
+
+//3003
+let fs = require("fs");
+let input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map((el) => Number(el));
+let origin = [1, 1, 2, 2, 2, 8];
+let result = origin.map((el, idx) => el - input[idx]);
+
+console.log(result.join(" "));
