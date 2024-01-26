5b79f8c0f87068872d87cc84bbe4e90fb769400d
unknown <pih9134@naver.com>
2024-01-26 19:52:21 +0900

    백준/2444

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -7,13 +7,29 @@
 // console.log("      |");
 
 //3003
-let fs = require("fs");
-let input = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split(" ")
-  .map((el) => Number(el));
-let origin = [1, 1, 2, 2, 2, 8];
-let result = origin.map((el, idx) => el - input[idx]);
+// let fs = require("fs");
+// let input = fs
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .split(" ")
+//   .map((el) => Number(el));
+// let origin = [1, 1, 2, 2, 2, 8];
+// let result = origin.map((el, idx) => el - input[idx]);
 
-console.log(result.join(" "));
+// console.log(result.join(" "));
+
+//2444
+let fs = require("fs");
+const N = Number(fs.readFileSync("input.txt"));
+
+for (let i = 1; i < N; i++) {
+  let blank = " ".repeat(N - i);
+  let stars = "*".repeat(i + (i - 1));
+  console.log(blank + stars);
+}
+
+for (let j = N; j > 0; j--) {
+  let blank = " ".repeat(N - j);
+  let stars = "*".repeat(j + (j - 1));
+  console.log(blank + stars);
+}
