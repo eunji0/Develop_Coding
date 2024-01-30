d351283eb6294641d5d8d2e455e724fdc77bec92
unknown <pih9134@naver.com>
2024-01-30 17:59:23 +0900

    백준/2903

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -252,26 +252,37 @@
 // console.log(Number(inputs[0]).toString(Number(inputs[1])).toUpperCase());
 
 //2720
+// var fs = require("fs");
+// const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// var input = fs
+//   .readFileSync(file)
+//   .toString()
+//   .split("\n")
+//   .map((v) => +v);
+
+// for (let i = 1; i < input.length; i++) {
+//   console.log(solution(input[i]));
+// }
+
+// function solution(change) {
+//   let count = [0, 0, 0, 0];
+//   const coins = [25, 10, 5, 1];
+
+//   for (let i = 0; i < coins.length; i++) {
+//     count[i] += Math.floor(change / coins[i]);
+//     change %= coins[i];
+//   }
+
+//   return count.join(" ");
+// }
+
+//2903
 var fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-var input = fs
-  .readFileSync(file)
-  .toString()
-  .split("\n")
-  .map((v) => +v);
+var input = fs.readFileSync(file).toString().trim();
 
-for (let i = 1; i < input.length; i++) {
-  console.log(solution(input[i]));
+let dot = 2;
+for (let i = 0; i < +input; i++) {
+  dot += Math.pow(2, i);
 }
-
-function solution(change) {
-  let count = [0, 0, 0, 0];
-  const coins = [25, 10, 5, 1];
-
-  for (let i = 0; i < coins.length; i++) {
-    count[i] += Math.floor(change / coins[i]);
-    change %= coins[i];
-  }
-
-  return count.join(" ");
-}
+console.log(Math.pow(dot, 2));
