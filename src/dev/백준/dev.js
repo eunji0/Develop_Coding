b3a7facec78dbf4f4e0cae7ba9f7ea21af4f7d3d
unknown <pih9134@naver.com>
2024-02-04 17:56:03 +0900

    백준/11651-좌표정렬하기2

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -742,3 +742,25 @@
 // };
 
 // console.log(solution(input));
+
+//11651-좌표정렬하기2
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split("\n")
+  .slice(1)
+  .map((el) => el.split(" ").map(Number));
+
+const solution = (input) => {
+  return input
+    .sort((a, b) => {
+      return a[1] === b[1] ? a[0] - b[0] : a[1] - b[1];
+    })
+    .map((el) => el.join(" "))
+    .join("\n");
+};
+
+console.log(solution(input));
