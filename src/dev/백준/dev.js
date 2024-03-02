2a5d3dc85fd1c1d8cdbec295cacd290dba4fe3f9
unknown <pih9134@naver.com>
2024-03-03 02:34:15 +0900

    백준/20920-영단어암기는 어려워

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -2169,53 +2169,88 @@
 // console.log(a.size);
 
 //2108-통계학
+// const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+// const input = require("fs").readFileSync(filePath).toString().split("\n");
+
+// let arr = input.map(Number);
+// let length = arr.shift();
+
+// // 산술평균
+// let a = arr.reduce((a, b) => a + b, 0);
+// let avg = Math.round(a / length);
+
+// // 중앙값
+// arr.sort((a, b) => a - b);
+// let answer =
+//   length % 2 === 0
+//     ? arr[length / 2] + arr[length / 2 - 1]
+//     : arr[Math.floor(length / 2)];
+
+// // 최빈값 데이터 집합에서 가장 많이 등장하는 값, 여러개 있을때 두번째로 작은값 출력
+// function getMostValue() {
+//   const map = new Map();
+//   for (let i in arr) {
+//     if (!map.has(arr[i])) {
+//       map.set(arr[i], 1); // key, value 저장
+//     } else {
+//       map.set(arr[i], map.get(arr[i]) + 1); // value + 1   중복되면 빈도수 추가, get은 value불러옴 아하
+//     }
+//   }
+//   let maxValue = 0;
+//   let arr2 = [];
+//   map.forEach((ele, key) => {
+//     if (maxValue < ele) {
+//       maxValue = ele;
+//       arr2 = []; // 배열 리셋
+//       arr2.push(key); // 반도수가 큰 것만 배열에 추가
+//     } else if (maxValue === map.get(key)) {
+//       arr2.push(key); // 빈도수가 똑같다면 전부 배열에 추가하기
+//     }
+//   });
+//   return arr2.length !== 1 ? arr2[1] : arr2[0]; // 배열의 길이가 1이 아니라면 두번째로 작은 최빈값 출력
+// }
+
+// // 범위
+// let max = Math.max(...arr);
+// let min = Math.min(...arr);
+// let range = max - min;
+
+// console.log(avg === -0 ? 0 : avg);
+// console.log(answer);
+// console.log(getMostValue());
+// console.log(range);
+
+//20920-영단어암기는 어려워
 const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
-const input = require("fs").readFileSync(filePath).toString().split("\n");
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .split("\n")
+  .map((v) => v.trim());
 
-let arr = input.map(Number);
-let length = arr.shift();
+let [N, M] = input[0].split(" ").map(Number);
 
-// 산술평균
-let a = arr.reduce((a, b) => a + b, 0);
-let avg = Math.round(a / length);
+input.shift();
 
-// 중앙값
-arr.sort((a, b) => a - b);
-let answer =
-  length % 2 === 0
-    ? arr[length / 2] + arr[length / 2 - 1]
-    : arr[Math.floor(length / 2)];
+let map = new Map();
 
-// 최빈값 데이터 집합에서 가장 많이 등장하는 값, 여러개 있을때 두번째로 작은값 출력
-function getMostValue() {
-  const map = new Map();
-  for (let i in arr) {
-    if (!map.has(arr[i])) {
-      map.set(arr[i], 1); // key, value 저장
-    } else {
-      map.set(arr[i], map.get(arr[i]) + 1); // value + 1   중복되면 빈도수 추가, get은 value불러옴 아하
-    }
-  }
-  let maxValue = 0;
-  let arr2 = [];
-  map.forEach((ele, key) => {
-    if (maxValue < ele) {
-      maxValue = ele;
-      arr2 = []; // 배열 리셋
-      arr2.push(key); // 반도수가 큰 것만 배열에 추가
-    } else if (maxValue === map.get(key)) {
-      arr2.push(key); // 빈도수가 똑같다면 전부 배열에 추가하기
-    }
-  });
-  return arr2.length !== 1 ? arr2[1] : arr2[0]; // 배열의 길이가 1이 아니라면 두번째로 작은 최빈값 출력
+for (let i = 0; i < input.length; i++) {
+  if (input[i].length >= M) map.set(input[i], (map.get(input[i]) || 0) + 1);
 }
 
-// 범위
-let max = Math.max(...arr);
-let min = Math.min(...arr);
-let range = max - min;
+map = [...map]
+  .sort((a, b) => {
+    if (a[1] === b[1]) {
+      if (a[0].length === b[0].length) {
+        return a[0] < b[0] ? -1 : 1;
+      } else {
+        return b[0].length - a[0].length;
+      }
+    } else {
+      return b[1] - a[1];
+    }
+  })
+  .map((v) => v[0])
+  .join("\n");
 
-console.log(avg === -0 ? 0 : avg);
-console.log(answer);
-console.log(getMostValue());
-console.log(range);
+console.log(map);
