3730e3174884e7ae3f20610a7c2bd00545d200b9
unknown <pih9134@naver.com>
2024-03-15 18:36:04 +0900

    백준//10986-나머지합

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -2881,45 +2881,65 @@ const e = require("express");
 // console.log(out.join("\n"));
 
 //16139-인간-컴퓨터 상호작용
-const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+// const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
 
-const [str, q, ...INPUT] = require("fs")
+// const [str, q, ...INPUT] = require("fs")
+//   .readFileSync(filePath)
+//   .toString()
+//   .trim()
+//   .split("\n");
+
+// let result = '';
+// let input=[];
+
+// INPUT.forEach(e=>{
+//   let temp = e.split(' ');
+//   temp[1] = +temp[1];
+//   temp[2]= +temp[2];
+
+//   input.push(temp);
+// })
+
+// const dp={};
+
+// input.forEach(e=>{
+//   if(!dp[e[0]]){
+//     let arr = [];
+
+//     for(let i=0; i<str.length; i++){
+//       let temp = i === 0 ? 0:arr[i-1];
+//       if(str[i]===e[0]) temp++;
+//       arr.push(temp)
+//     }
+
+//     dp[e[0]] = arr
+//   }
+
+//   if(e[1] === 0){
+//     result+=dp[e[0]][e[2]]+'\n';
+//   }else{
+//     result+=dp[e[0]][e[2]] - dp[e[0]][e[1]-1]+'\n'
+//   }
+// })
+
+// console.log(result)
+
+//10986-나머지합
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+const input = require("fs")
   .readFileSync(filePath)
   .toString()
   .trim()
   .split("\n");
 
-let result = '';
-let input=[];
+const [n, m] = input[0].split(" ").map(Number);
+const a = input[1].split(" ").map(Number);
 
-INPUT.forEach(e=>{
-  let temp = e.split(' ');
-  temp[1] = +temp[1];
-  temp[2]= +temp[2];
+const [b, c] = [Array(n).fill(0), Array(m).fill(0n)];
 
-  input.push(temp);
-})
-
-const dp={};
-
-input.forEach(e=>{
-  if(!dp[e[0]]){
-    let arr = [];
-
-    for(let i=0; i<str.length; i++){
-      let temp = i === 0 ? 0:arr[i-1];
-      if(str[i]===e[0]) temp++;
-      arr.push(temp)
-    }
-
-    dp[e[0]] = arr
-  }
-
-  if(e[1] === 0){
-    result+=dp[e[0]][e[2]]+'\n';
-  }else{
-    result+=dp[e[0]][e[2]] - dp[e[0]][e[1]-1]+'\n'
-  }
-})
-
-console.log(result)
+for (let i = 0; i < n; i++) {
+  b[i] = ((b[i - 1] ?? 0) + a[i]) % m;
+}
+b.forEach((e) => c[e]++);
+const o = c[0] + c.reduce((a, c) => a + (c * (c - 1n)) / 2n, 0n);
+console.log(o.toString());
