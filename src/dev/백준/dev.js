e36f1e45c03ff5cc65f9cddaa879b8d2bef39541
unknown <pih9134@naver.com>
2024-03-26 00:47:44 +0900

    백준//1654-랜선자르기

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -3739,29 +3739,57 @@
 // );
 
 //2630-색종이 만들기
-const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-const n = +input[0];
-const paper = input.slice(1).map(v => v.split(" ").map(vv => +vv));
-const countPaper = n => {
-    const count = [0, 0];
-    const recursion = (n, x, y) => {
-        let total = 0;
-        for(let i=0; i<n; i++) {
-            for(let j=0; j<n; j++) {
-                total += paper[y+j][x+i];
-            }
-        }
-        if (total === 0) count[0]++;
-        else if (total === n*n) count[1]++;
-        else {
-            n /= 2;
-            recursion(n, x, y);
-            recursion(n, x+n, y);
-            recursion(n, x, y+n);
-            recursion(n, x+n, y+n);
-        }
-    }
-    recursion(n, 0, 0);
-    console.log(count.join("\n"));
-};
-countPaper(n);
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+// const n = +input[0];
+// const paper = input.slice(1).map(v => v.split(" ").map(vv => +vv));
+// const countPaper = n => {
+//     const count = [0, 0];
+//     const recursion = (n, x, y) => {
+//         let total = 0;
+//         for(let i=0; i<n; i++) {
+//             for(let j=0; j<n; j++) {
+//                 total += paper[y+j][x+i];
+//             }
+//         }
+//         if (total === 0) count[0]++;
+//         else if (total === n*n) count[1]++;
+//         else {
+//             n /= 2;
+//             recursion(n, x, y);
+//             recursion(n, x+n, y);
+//             recursion(n, x, y+n);
+//             recursion(n, x+n, y+n);
+//         }
+//     }
+//     recursion(n, 0, 0);
+//     console.log(count.join("\n"));
+// };
+// countPaper(n);
+
+//1654-랜선자르기
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, k] = input
+  .shift()
+  .split(" ")
+  .map((a) => +a);
+const lines = input.map((a) => +a).sort();
+let max = Math.max(...lines);
+let min = 1;
+
+while (min <= max) {
+  let mid = parseInt((max + min) / 2);
+  let howManyPieces = lines
+    .map((line) => parseInt(line / mid))
+    .reduce((a, b) => a + b, 0);
+  if (howManyPieces >= k) {
+    min = mid + 1;
+  } else {
+    max = mid - 1;
+  }
+}
+console.log(max);
