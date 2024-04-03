41e34b5a3a82eaa311d135eb1f00292d8a6ce4c8
unknown <pih9134@naver.com>
2024-04-04 01:28:41 +0900

    백준//2580-스도쿠

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -4003,35 +4003,93 @@
 // console.log(solution(n, m))
 
 //9663 - N-Queen
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+// const input = fs.readFileSync(filePath).toString().trim();
+// const N = +input;
+
+// let answer = 0;
+// let row = Array.from({ length: N + 1 }, () => 0);
+
+// function check(L) {
+//   for (let i = 0; i < L; i++) {
+//     if (row[L] === row[i] || L - i === Math.abs(row[L] - row[i])) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function DFS(L) {
+//   if (L === N) {
+//     answer++;
+//   } else {
+//     for (let i = 0; i < N; i++) {
+//       row[L] = i;
+//       if (check(L)) {
+//         DFS(L + 1);
+//       }
+//     }
+//   }
+// }
+
+// DFS(0);
+// console.log(answer);
+
+//2580-스도쿠
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString().trim();
-const N = +input;
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+input = input.map(row => row.split(' ').map(i => Number(i)));
 
-let answer = 0;
-let row = Array.from({ length: N + 1 }, () => 0);
-
-function check(L) {
-  for (let i = 0; i < L; i++) {
-    if (row[L] === row[i] || L - i === Math.abs(row[L] - row[i])) {
-      return false;
-    }
-  }
-  return true;
-}
+let zeroCoords = getZeroCoords();
+let N = zeroCoords.length;
+let answer = "";
+DFS(0);
 
 function DFS(L) {
   if (L === N) {
-    answer++;
+    for (let x of input) {
+      answer += `${x.join(" ")}\n`;
+    }
+    console.log(answer);
+    process.exit(0);
   } else {
-    for (let i = 0; i < N; i++) {
-      row[L] = i;
-      if (check(L)) {
+    let [row, col] = zeroCoords[L];
+
+    for (let i = 1; i <= input.length; i++) {
+      if (check(row, col, i)) {
+        input[row][col] = i;
         DFS(L + 1);
+        input[row][col] = 0;
       }
     }
   }
 }
 
-DFS(0);
-console.log(answer);
+function check(row, col, value) {
+  let threeRow = Math.floor(row / 3) * 3;
+  let threeCol = Math.floor(col / 3) * 3;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[row][i] === value || input[i][col] === value) return false;
+  }
+
+  for (let i = threeRow; i < threeRow + 3; i++) {
+    for (let j = threeCol; j < threeCol + 3; j++) {
+      if (input[i][j] === value) return false;
+    }
+  }
+
+  return true;
+}
+
+function getZeroCoords() {
+  const arr = [];
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      if (input[i][j] === 0) arr.push([i, j]);
+    }
+  }
+  return arr;
+}
