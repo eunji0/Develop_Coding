2e5f4378d5b5c998cfc439b4f99967428406a26d
unknown <pih9134@naver.com>
2024-04-18 01:16:04 +0900

    백준//1654-랜선자르기

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -4352,80 +4352,101 @@
 //  console.log(result)
 
 //10830-행렬 제곱
-const fs = require('fs');
-const stdin = (process.platform === 'linux'
-        ? fs.readFileSync('/dev/stdin').toString()
-        : `2 5
-1 2
-3 4`
-).split('\n');
+// const fs = require('fs');
+// const stdin = (process.platform === 'linux'
+//         ? fs.readFileSync('/dev/stdin').toString()
+//         : `2 5
+// 1 2
+// 3 4`
+// ).split('\n');
 
-const input = (() => {
-    let line = 0;
-    return () => stdin[line++];
-})();
+// const input = (() => {
+//     let line = 0;
+//     return () => stdin[line++];
+// })();
 
-function mulMat(mata, matb){
-    // tmpmat init
-    let tmpmat = new Array(size);
-    for(let i=0; i<size; i++){
-        tmpmat[i] = new Array(size);
-    }
-    for(let i=0; i<size; i++){
-        for(let j=0; j<size; j++){
-            tmpmat[i][j] = 0;
-        }
-    }
+// function mulMat(mata, matb){
+//     // tmpmat init
+//     let tmpmat = new Array(size);
+//     for(let i=0; i<size; i++){
+//         tmpmat[i] = new Array(size);
+//     }
+//     for(let i=0; i<size; i++){
+//         for(let j=0; j<size; j++){
+//             tmpmat[i][j] = 0;
+//         }
+//     }
 
-    // mul
-    for(let i=0; i<size; i++){
-        for(let j=0; j<size; j++){
-            for(let k=0; k<size; k++){
-                tmpmat[i][j] += mata[i][k] * matb[k][j];
-            }
-            tmpmat[i][j] %= 1000;
-        }
-    }
+//     // mul
+//     for(let i=0; i<size; i++){
+//         for(let j=0; j<size; j++){
+//             for(let k=0; k<size; k++){
+//                 tmpmat[i][j] += mata[i][k] * matb[k][j];
+//             }
+//             tmpmat[i][j] %= 1000;
+//         }
+//     }
 
-    return tmpmat;
+//     return tmpmat;
+// }
+
+// // init
+// const f = input().split(' ').map(Number);
+// const size = f[0];
+// let pow = f[1];
+// let matrix = [];
+
+// for(let i=0; i<size; i++){
+//     matrix.push(input().split(' ').map(Number)); //deep copy
+// }
+
+// // unit matrix
+// let tmpmat = new Array(size);
+// for(let i=0; i<size; i++){
+//     tmpmat[i] = new Array(size);
+// }
+// for(let i=0; i<size; i++){
+//     for(let j=0; j<size; j++){
+//         tmpmat[i][j] = 0;
+//     }
+//     tmpmat[i][i] = 1;
+// }
+
+// while(pow){
+//     if(pow % 2 == 1){
+//         tmpmat = mulMat(tmpmat, matrix);
+//         pow--;
+//     }
+//     pow /= 2;
+//     matrix = mulMat(matrix, matrix);
+// }
+
+// // answer
+// for(let i=0; i<size; i++){
+//     let tmp = "";
+//     for(let j=0; j<size; j++){
+//         tmp += tmpmat[i][j] + " ";
+//     }
+//     console.log(tmp);
+// }
+
+//1654-랜선자르기
+const input = require("fs").readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, k] = input.shift().split(' ').map((a) => +a);
+	const lines = input.map((a) => +a).sort();
+	let max = Math.max(...lines);
+	let min = 1;
+
+while (min <= max) {
+  let mid = parseInt((max + min) / 2);
+  let howManyPieces = lines
+    .map((line) => parseInt(line / mid))
+    .reduce((a, b) => a + b, 0);
+  if (howManyPieces >= k) {
+    min = mid + 1;
+  } else {
+    max = mid - 1;
+  }
 }
-
-// init
-const f = input().split(' ').map(Number);
-const size = f[0];
-let pow = f[1];
-let matrix = [];
-
-for(let i=0; i<size; i++){
-    matrix.push(input().split(' ').map(Number)); //deep copy
-}
-
-// unit matrix
-let tmpmat = new Array(size);
-for(let i=0; i<size; i++){
-    tmpmat[i] = new Array(size);
-}
-for(let i=0; i<size; i++){
-    for(let j=0; j<size; j++){
-        tmpmat[i][j] = 0;
-    }
-    tmpmat[i][i] = 1;
-}
-
-while(pow){
-    if(pow % 2 == 1){
-        tmpmat = mulMat(tmpmat, matrix);
-        pow--;
-    }
-    pow /= 2;
-    matrix = mulMat(matrix, matrix);
-}
-
-// answer
-for(let i=0; i<size; i++){
-    let tmp = "";
-    for(let j=0; j<size; j++){
-        tmp += tmpmat[i][j] + " ";
-    }
-    console.log(tmp);
-}
+console.log(max);
