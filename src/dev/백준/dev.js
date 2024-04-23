58bbf8d2c98800512262328191458d419a82db0d
unknown <pih9134@naver.com>
2024-04-24 01:01:27 +0900

    백준//11279-최대힙

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -4822,28 +4822,121 @@
 // console.log(sol(arr, m, 0, Math.max(...arr)))
 
 //2110-공유기 설치
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+// const input = fs.readFileSync(filePath).toString().trim().split("\n");
+// const [n, m]=input[0].split(' ').map(Number);
+// const arr = input.slice(1).map(Number).sort((a, b)=>a-b);
+
+// let start = 1;
+// let end = arr[arr.length-1];
+
+// while(start <= end){
+//     let mid = Math.floor((start+end)/2);
+
+//     let count=1;
+//     let prev = arr[0];
+//     for(let cur of arr){
+//         if(cur-prev<mid) continue;
+//         prev=cur
+//         count+=1;
+//     }
+
+//     if(count<m) end=mid-1;
+//     else start = mid+1;
+// }
+
+// console.log(end)
+
+//11279-최대힙
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
-const [n, m]=input[0].split(' ').map(Number);
-const arr = input.slice(1).map(Number).sort((a, b)=>a-b);
-
-let start = 1;
-let end = arr[arr.length-1];
-
-while(start <= end){
-    let mid = Math.floor((start+end)/2);
-
-    let count=1;
-    let prev = arr[0];
-    for(let cur of arr){
-        if(cur-prev<mid) continue;
-        prev=cur
-        count+=1;
-    }
-
-    if(count<m) end=mid-1;
-    else start = mid+1;
+let operations = []
+for (let i = 1; i < input.length; i++) {
+    operations.push(+input[i])
 }
 
-console.log(end)
+class maxHeap {
+    constructor() {
+        this.nodes = []
+    }
+
+    insert(data) {
+        this.nodes.push(data)
+        this.bubbleUp()
+    }
+
+    bubbleUp(index = this.nodes.length - 1) {
+        if (index < 1) return
+        let currentNode = this.nodes[index]
+        let parentIndex = Math.floor((index - 1) / 2)
+        let parentNode = this.nodes[parentIndex]
+
+        if (parentNode >= currentNode) return
+
+        this.nodes[parentIndex] = currentNode
+        this.nodes[index] = parentNode
+        index = parentIndex
+        this.bubbleUp(index)
+    }
+
+    extract() {
+        const max = this.nodes[0]
+        if (this.nodes.length === 1) {
+            this.nodes.pop()
+            return max
+        }
+        this.nodes[0] = this.nodes.pop()
+        this.trickleDown()
+        return max
+    }
+
+    trickleDown(index = 0) {
+        let leftChildIndex = index * 2 + 1
+        let rightChildIndex = index * 2 + 2
+        let length = this.nodes.length
+        let maximum = index
+
+        if (!this.nodes[leftChildIndex] && !this.nodes[rightChildIndex]) return
+
+        if (!this.nodes[rightChildIndex]) {
+            if (this.nodes[leftChildIndex] > this.nodes[maximum]) {
+                maximum = leftChildIndex
+            }
+        }
+
+        if (this.nodes[leftChildIndex] < this.nodes[rightChildIndex]) {
+            if (rightChildIndex <= length && this.nodes[rightChildIndex] > this.nodes[maximum]) {
+                maximum = rightChildIndex
+            }
+        } else {
+            if (leftChildIndex <= length && this.nodes[leftChildIndex] > this.nodes[maximum]) {
+                maximum = leftChildIndex
+            }
+        }
+
+        if (maximum !== index) {
+            let t = this.nodes[maximum]
+            this.nodes[maximum] = this.nodes[index]
+            this.nodes[index] = t
+            this.trickleDown(maximum)
+        }
+    }
+}
+const heap = new maxHeap()
+let extracts = ''
+operations.forEach((operation, index) => {
+    if (operation !== 0) {
+        heap.insert(operation)
+    } else {
+        if (heap.nodes.length === 0) {
+            extracts += "0" + "\n"
+        } else {
+            let t = heap.extract()
+            extracts += t + "\n"
+        }
+    }
+})
+
+console.log(extracts.trim())
