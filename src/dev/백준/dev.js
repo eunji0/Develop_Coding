b10b94e229a0e47f190235fc68b01b0271e515a5
unknown <pih9134@naver.com>
2024-05-04 23:33:34 +0900

    백준//11000-강의실 배정

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -5640,83 +5640,115 @@
 // fs.writeFileSync("output.txt", output.join("\n"));
 
 //1715-카드 정렬하기
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
+// class MinHeap {
+//   constructor() {
+//     this.heap = [];
+//   }
 
-  getLength = () => {
-    return this.heap.length;
-  };
+//   getLength = () => {
+//     return this.heap.length;
+//   };
 
-  push = (node) => {
-    this.heap.push(node);
-    let i = this.heap.length - 1;
-    let parentI = Math.floor((i - 1) / 2);
-    while (i > 0 && this.heap[parentI] > this.heap[i]) {
-      this.swap(i, parentI);
-      i = parentI;
-      parentI = Math.floor((i - 1) / 2);
-    }
-  };
+//   push = (node) => {
+//     this.heap.push(node);
+//     let i = this.heap.length - 1;
+//     let parentI = Math.floor((i - 1) / 2);
+//     while (i > 0 && this.heap[parentI] > this.heap[i]) {
+//       this.swap(i, parentI);
+//       i = parentI;
+//       parentI = Math.floor((i - 1) / 2);
+//     }
+//   };
 
-  pop = () => {
-    if (this.heap.length === 1) {
-      return this.heap.pop();
-    }
+//   pop = () => {
+//     if (this.heap.length === 1) {
+//       return this.heap.pop();
+//     }
 
-    const result = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    let i = 0;
-    while (true) {
-      const leftI = i * 2 + 1,
-        rightI = i * 2 + 2;
-      if (leftI >= this.heap.size) {
-        break;
-      }
-      let nextI = i;
-      if (this.heap[nextI] > this.heap[leftI]) {
-        nextI = leftI;
-      }
-      if (rightI < this.heap.length && this.heap[nextI] > this.heap[rightI]) {
-        nextI = rightI;
-      }
-      if (nextI === i) {
-        break;
-      }
-      this.swap(i, nextI);
-      i = nextI;
-    }
-    return result;
-  };
+//     const result = this.heap[0];
+//     this.heap[0] = this.heap.pop();
+//     let i = 0;
+//     while (true) {
+//       const leftI = i * 2 + 1,
+//         rightI = i * 2 + 2;
+//       if (leftI >= this.heap.size) {
+//         break;
+//       }
+//       let nextI = i;
+//       if (this.heap[nextI] > this.heap[leftI]) {
+//         nextI = leftI;
+//       }
+//       if (rightI < this.heap.length && this.heap[nextI] > this.heap[rightI]) {
+//         nextI = rightI;
+//       }
+//       if (nextI === i) {
+//         break;
+//       }
+//       this.swap(i, nextI);
+//       i = nextI;
+//     }
+//     return result;
+//   };
 
-  swap = (a, b) => {
-    const temp = this.heap[a];
-    this.heap[a] = this.heap[b];
-    this.heap[b] = temp;
-  };
+//   swap = (a, b) => {
+//     const temp = this.heap[a];
+//     this.heap[a] = this.heap[b];
+//     this.heap[b] = temp;
+//   };
+// }
+
+// const input = [];
+
+// require('readline')
+//   .createInterface(process.stdin, process.stdout)
+//   .on('line', function (line) {
+//     input.push(line.trim());
+//   })
+//   .on('close', function () {
+//     const minHeap = new MinHeap();
+//     for (let i = 1; i < input.length; i++) {
+//       minHeap.push(+input[i]);
+//     }
+
+//     let totalCompareCount = 0;
+//     while (minHeap.getLength() > 1) {
+//       let aCount = minHeap.pop();
+//       let bCount = minHeap.pop();
+//       let compareCount = aCount + bCount;
+//       totalCompareCount += compareCount;
+//       minHeap.push(compareCount);
+//     }
+//     console.log(totalCompareCount);
+//   });
+
+//11000-강의실 배정
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+input.shift();
+
+const times = [];
+
+input.forEach((v) => {
+  v = v.split(" ");
+  v = v.map((v) => parseInt(v));
+  times.push([v[0], 1]);
+  times.push([v[1], -1]);
+});
+
+times.sort((a, b) => {
+  if (a[0] === b[0]) return a[1] - b[1];
+  return a[0] - b[0];
+});
+
+let answer = 0;
+let result = 0;
+
+for (let i = 0; i < times.length; i++) {
+  result += times[i][1];
+  answer = Math.max(result, answer);
 }
 
-const input = [];
-
-require('readline')
-  .createInterface(process.stdin, process.stdout)
-  .on('line', function (line) {
-    input.push(line.trim());
-  })
-  .on('close', function () {
-    const minHeap = new MinHeap();
-    for (let i = 1; i < input.length; i++) {
-      minHeap.push(+input[i]);
-    }
-
-    let totalCompareCount = 0;
-    while (minHeap.getLength() > 1) {
-      let aCount = minHeap.pop();
-      let bCount = minHeap.pop();
-      let compareCount = aCount + bCount;
-      totalCompareCount += compareCount;
-      minHeap.push(compareCount);
-    }
-    console.log(totalCompareCount);
-  });
+console.log(answer);
