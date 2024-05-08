e04f8a4edd4f1b1d17e09003e479d5fce7ea3264
unknown <pih9134@naver.com>
2024-05-09 00:40:07 +0900

    백준//24444-너비우선탐색1

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -5900,35 +5900,83 @@
 // console.log(visited.join('\n'));
 
 //24480-깊이 우선 탐색2
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-const [N, M, R] = input.shift().split(' ').map(Number);
-const arr = input.map((v) => v.split(' ').map(Number));
-const graph = [...Array(N + 1)].map(() => []);
-const visited = Array(N).fill(0);
-let cnt = 1;
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+// const [N, M, R] = input.shift().split(' ').map(Number);
+// const arr = input.map((v) => v.split(' ').map(Number));
+// const graph = [...Array(N + 1)].map(() => []);
+// const visited = Array(N).fill(0);
+// let cnt = 1;
 
-// 무방향(양방향) 그래프 만들기
-arr.map(([from, to]) => {
-  graph[from].push(to);
-  graph[to].push(from);
-});
+// // 무방향(양방향) 그래프 만들기
+// arr.map(([from, to]) => {
+//   graph[from].push(to);
+//   graph[to].push(from);
+// });
 
-// 내림차순 정렬
-graph.map((v) => v.sort((a, b) => b - a));
+// // 내림차순 정렬
+// graph.map((v) => v.sort((a, b) => b - a));
 
-// DFS
-const dfs = (node) => {
-  // graph의 0번째 인덱스는 쓰지 않으므로 현재 노드번호에 -1의 방문여부 체크
-  if (!visited[node - 1]) {
-  	// 방문 체크함과 동시에 해당 인덱스의 값은 1씩 증가하는 cnt로 넣어준다.
-    visited[node - 1] = cnt;
-    cnt++;
-    for (const next of graph[node]) dfs(next);
-  }
-};
+// // DFS
+// const dfs = (node) => {
+//   // graph의 0번째 인덱스는 쓰지 않으므로 현재 노드번호에 -1의 방문여부 체크
+//   if (!visited[node - 1]) {
+//   	// 방문 체크함과 동시에 해당 인덱스의 값은 1씩 증가하는 cnt로 넣어준다.
+//     visited[node - 1] = cnt;
+//     cnt++;
+//     for (const next of graph[node]) dfs(next);
+//   }
+// };
 
-dfs(R);
+// dfs(R);
 
-console.log(visited.join('\n'));
+// console.log(visited.join('\n'));
 
+//24444-너비우선탐색1
+const input = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
+
+class Que {
+    q = [];
+    h = 0;
+    t = 0;
+    enque(v) {
+        this.q[this.t++] = v;
+    }
+    deque() {
+        const v = this.q[this.h];
+        delete this.q[this.h++];
+        return v;
+    }
+    size() {
+        return this.t - this.h;
+    }
+}
+
+const [n, m, r] = input[0].split(" ").map(Number);
+const graph = Array.from({ length: n + 1 }, () => []);
+for (let i = 1; i <= m; i++) {
+    const [a, b] = input[i].split(" ").map(Number);
+    graph[a].push(b);
+    graph[b].push(a);
+}
+graph.forEach((line) => line.sort((a, b) => a - b));
+// console.log(graph);
+const visited = new Array(n + 1).fill(0);
+
+const queue = new Que();
+queue.enque(r);
+visited[r] = 1;
+
+let step = 1;
+while (queue.size()) {
+    const cur = queue.deque();
+
+    for (const next of graph[cur]) {
+        if (!visited[next]) {
+            queue.enque(next);
+            visited[next] = ++step;
+        }
+    }
+}
+
+console.log(visited.slice(1).join("\n"));
