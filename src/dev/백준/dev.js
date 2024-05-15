4fd0fd8ceac921771399acf6422177073d971ee2
unknown <pih9134@naver.com>
2024-05-16 00:13:11 +0900

    백준//2178-미로탐색

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -6140,52 +6140,94 @@
 // answer.forEach((item) => console.log(item));
 
 //1012-유기농 배추
-const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-const num = Number(input.shift());
-const ds = [
-  [-1, 0],
-  [1, 0],
-  [0, 1],
-  [0, -1],
-];
-function bfs(startX, startY) {
-  //시작 좌표 기준으로 시작
-  const queue = [[startX, startY]];
-  // queue가 비워지면 탈출
-  while (queue.length) {
-    const [x, y] = queue.shift();
-    // queue의 값을 하나씩 빼서 xy로 저장
-    // xy좌표가 0 이면 다시, 1이면 0으로 만들어준다.
-    // 인접한 1들 다 0으로 만들기
-    if (!map[x][y]) continue;
-    else map[x][y] = 0;
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+// const num = Number(input.shift());
+// const ds = [
+//   [-1, 0],
+//   [1, 0],
+//   [0, 1],
+//   [0, -1],
+// ];
+// function bfs(startX, startY) {
+//   //시작 좌표 기준으로 시작
+//   const queue = [[startX, startY]];
+//   // queue가 비워지면 탈출
+//   while (queue.length) {
+//     const [x, y] = queue.shift();
+//     // queue의 값을 하나씩 빼서 xy로 저장
+//     // xy좌표가 0 이면 다시, 1이면 0으로 만들어준다.
+//     // 인접한 1들 다 0으로 만들기
+//     if (!map[x][y]) continue;
+//     else map[x][y] = 0;
 
-    // 상하좌우 탐색하여 1이 있다면 queue에 push 해준다.
-    for (let i = 0; i < 4; i++) {
-      const xPos = x + ds[i][0];
-      const yPos = y + ds[i][1];
+//     // 상하좌우 탐색하여 1이 있다면 queue에 push 해준다.
+//     for (let i = 0; i < 4; i++) {
+//       const xPos = x + ds[i][0];
+//       const yPos = y + ds[i][1];
 
-      if (xPos < 0 || yPos < 0 || xPos >= M || yPos >= N) continue;
-      if (map[xPos][yPos]) queue.push([xPos, yPos]);
-    }
-  }
-}
-for (let i = 0; i < num; i++) {
-  let worm = 0;
-  var [M, N, K] = input.shift().split(" ").map(Number);
-  var map = Array.from(Array(M), () => new Array(N).fill(0));
-  for (let j = 0; j < K; j++) {
-    let xy = input.shift().split(" ");
-    map[xy[0]][xy[1]] = 1;
-  }
-  for (let k = 0; k < M; k++) {
-    for (let l = 0; l < N; l++) {
-      //만약 그 좌표가 1이라면 worm을 늘려주고 상하좌우 탐색하여 전부 0으로 만들어준다.
-      if (map[k][l]) {
-        bfs(k, l);
-        worm++;
+//       if (xPos < 0 || yPos < 0 || xPos >= M || yPos >= N) continue;
+//       if (map[xPos][yPos]) queue.push([xPos, yPos]);
+//     }
+//   }
+// }
+// for (let i = 0; i < num; i++) {
+//   let worm = 0;
+//   var [M, N, K] = input.shift().split(" ").map(Number);
+//   var map = Array.from(Array(M), () => new Array(N).fill(0));
+//   for (let j = 0; j < K; j++) {
+//     let xy = input.shift().split(" ");
+//     map[xy[0]][xy[1]] = 1;
+//   }
+//   for (let k = 0; k < M; k++) {
+//     for (let l = 0; l < N; l++) {
+//       //만약 그 좌표가 1이라면 worm을 늘려주고 상하좌우 탐색하여 전부 0으로 만들어준다.
+//       if (map[k][l]) {
+//         bfs(k, l);
+//         worm++;
+//       }
+//     }
+//   }
+//   console.log(worm);
+// }
+
+
+//2178-미로탐색
+const sol = (input) => {
+    const [N, M] = input[0].split(" ").map((v) => +v);
+    const adjM = [];
+    for (let i = 1; i <= N; i++) adjM.push(input[i].split("").map((v) => +v)); // 미로 행렬
+    const check = Array.from({ length: N }, () => Array(M).fill(0)); // 방문 여부를 위한 체크 행렬
+  
+    function bfs(row, col) {
+      const dx = [-1, 0, 1, 0];
+      const dy = [0, 1, 0, -1]; // 현재 위치로부터 동서남북 조회를 위한 dx, dy 배열
+      const queue = [];
+      queue.push([row, col]);
+      check[row][col] = 1;
+      while (queue.length) {
+        const [x, y] = queue.shift(); // 큐는 FIFO이므로, 맨 앞부터 꺼낸다.
+        for (let i = 0; i < 4; i++) {
+          const [nx, ny] = [x + dx[i], y + dy[i]]; // (nx, ny)는 이동 가능성이 있는 좌표.
+          if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue; // 미로를 벗어나는 좌표는 제외
+          if (adjM[nx][ny] && !check[nx][ny]) { // 길이 있고, 방문하지 않았다면 방문
+            check[nx][ny] = check[x][y] + 1; // (x,y)의 값이 (x,y)까지 최단경로에 해당한다.
+            queue.push([nx, ny]); // BFS(너비 우선)로 현재 위치에서 갈 수 있는 좌표를 모두 큐에 넣어준다.
+          }
+        }
       }
     }
-  }
-  console.log(worm);
-}
+    bfs(0, 0);
+    return check[N - 1][M - 1];
+  };
+  
+  // 백준에서 입력을 받기 위한 코드
+  const input = [];
+  require("readline")
+    .createInterface(process.stdin, process.stdout)
+    .on("line", (line) => {
+      input.push(line);
+    })
+    .on("close", () => {
+      console.log(sol(input));
+      process.exit();
+    });
