c851091152dab183784063c8331fd8b38e6430d3
unknown <pih9134@naver.com>
2024-05-18 00:53:50 +0900

    스택과 큐//10773-제로

--- src/dev/백준/스택과 큐.js
+++ src/dev/백준/스택과 큐.js
@@ -1,49 +1,82 @@
 // 스택 - (Last In First Out)LIFO
 // 큐 - (First In First Out)FIFO 
 
-class Stack {
-  constructor(){
+// class Stack {
+//   constructor(){
+//     this.arr = [];
+//   }
+
+//   push(data){
+//     this.arr.push(data)
+//   }
+
+//   pop(index){
+//     if(index === this.arr.length-1){
+//       return this.arr.pop();
+//     }
+
+//     let result = this.arr.splice(index, 1);
+//     return result
+//   }
+
+//   empty(){
+//     if(this.arr.length===0){
+//       return true;
+//     }else{
+//       return false;
+//     }
+//   }
+
+//   top(){
+//     return this.arr[this.arr.length-1]
+//   }
+
+//   bottom(){
+//     return this.arr[0];
+//   }
+
+// }
+
+// let s = new Stack();
+// s.push(10);
+// s.push(20);
+// s.push(30);
+// s.push(100);
+// s.push(200);
+// s.push(300);
+// let popValue = s.pop(2)
+// console.log(s);
+// console.log(popValue)
+
+//10773-제로
+class Zero {
+  constructor() {
     this.arr = [];
   }
 
-  push(data){
-    this.arr.push(data)
+  push(data) {
+    return this.arr.push(data);
   }
 
-  pop(index){
-    if(index === this.arr.length-1){
-      return this.arr.pop();
-    }
-
-    let result = this.arr.splice(index, 1);
-    return result
+  pop() {
+    return this.arr.pop();
   }
-
-  empty(){
-    if(this.arr.length===0){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  top(){
-    return this.arr[this.arr.length-1]
-  }
-
-  bottom(){
-    return this.arr[0];
-  }
-
 }
 
-let s = new Stack();
-s.push(10);
-s.push(20);
-s.push(30);
-s.push(100);
-s.push(200);
-s.push(300);
-let popValue = s.pop(2)
-console.log(s);
-console.log(popValue)
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [n, ...arr] = input.map(Number);
+
+let array = new Zero();
+
+arr.forEach(v => {
+  if (v === 0) {
+    array.pop();
+  } else {
+    array.push(v);
+  }
+});
+
+let sum = array.arr.reduce((a, c) => a + c, 0);
+console.log(sum);
+
