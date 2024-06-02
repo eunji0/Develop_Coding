cc664c6479cdfcce42d2bd35d9ee3620bbd6b6d5
unknown <pih9134@naver.com>
2024-06-03 00:38:44 +0900

    bfs//1916-최소비용 순차탐색구현

--- src/dev/백준/dfs와 bfs.js
+++ src/dev/백준/dfs와 bfs.js
@@ -158,252 +158,280 @@
 //최단 경로 문제 => 최소힙+다익스트라 알고리즘
 
 //먼저 최소힙 이해하기
-class MinHeap{
-  constructor(){
-    this.heap=[];
-  }
+// class MinHeap{
+//   constructor(){
+//     this.heap=[];
+//   }
 
-  push(value){
-    this.heap.push(value);
-    this.heapifyUp();
-  }
+//   push(value){
+//     this.heap.push(value);
+//     this.heapifyUp();
+//   }
 
-  //새로운 노드 자리 찾아주기
-  heapifyUp(){
-    let index = this.heap.length-1;
+//   //새로운 노드 자리 찾아주기
+//   heapifyUp(){
+//     let index = this.heap.length-1;
 
-    //인덱스가 부모노드가 될때까지
-    while(index>0){
-      const parentIndex = Math.floor((index-1)/2);
+//     //인덱스가 부모노드가 될때까지
+//     while(index>0){
+//       const parentIndex = Math.floor((index-1)/2);
 
-      if(this.heap[parentIndex]<=this.heap[index]) break;
+//       if(this.heap[parentIndex]<=this.heap[index]) break;
 
-      [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+//       [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
 
-      index = parentIndex;
-    }
-  }
+//       index = parentIndex;
+//     }
+//   }
 
-  pop(){
-    if(this.isEmpty()) return null;
+//   pop(){
+//     if(this.isEmpty()) return null;
 
-    const root = this.heap[0];
-    const lastNode = this.heap.pop();
+//     const root = this.heap[0];
+//     const lastNode = this.heap.pop();
 
-    if(!this.isEmpty()){
-      this.heap[0] = lastNode;
-      this.heapifyDown();
-    }
+//     if(!this.isEmpty()){
+//       this.heap[0] = lastNode;
+//       this.heapifyDown();
+//     }
 
-    return root
-  }
+//     return root
+//   }
 
-  //루트 노드 제거후 재정비
-  heapifyDown(){
-    let index =0;
-    const length = this.heap.length;
+//   //루트 노드 제거후 재정비
+//   heapifyDown(){
+//     let index =0;
+//     const length = this.heap.length;
 
-    while(true){
-      let smallest = index;
-      const leftChildIndex = index*2+1;
-      const rigthChildIndex = index*2+2;
+//     while(true){
+//       let smallest = index;
+//       const leftChildIndex = index*2+1;
+//       const rigthChildIndex = index*2+2;
 
-      if(leftChildIndex < length &&this.heap[leftChildIndex] <this.heap[smallest]){
-        smallest = leftChildIndex;
-      }
+//       if(leftChildIndex < length &&this.heap[leftChildIndex] <this.heap[smallest]){
+//         smallest = leftChildIndex;
+//       }
 
-      if(rigthChildIndex <length &&this.heap[rigthChildIndex] < this.heap[smallest]){
-        smallest = rigthChildIndex;
-      }
+//       if(rigthChildIndex <length &&this.heap[rigthChildIndex] < this.heap[smallest]){
+//         smallest = rigthChildIndex;
+//       }
 
-      if(smallest === index) break;
+//       if(smallest === index) break;
 
-      [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]]
-      index = smallest
-    }
+//       [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]]
+//       index = smallest
+//     }
     
-  }
-}
+//   }
+// }
 
-//특정한 하나의 정점에서 다른 모든 정점으로 가는 최단 경로를 계산 => 다익스트라 알고리즘
+// //특정한 하나의 정점에서 다른 모든 정점으로 가는 최단 경로를 계산 => 다익스트라 알고리즘
 
-//작동 이해하기
-//노드 간의 거리는 Infinity로 저장 => 무한대의 값을 저장해서 영원히 닿을 수 없다는 의미
-//출발 노드는 1번 노드
-//1번 노드는 자기 스스로와 거리가 0 이기 때문에 먼저 0 으로 채워넣는다.
-//1번 노드 방문했으니 visited=true로 설정
-//다음에 방문할 노드 찾기(거리 값이 작은 걸로 선택)
-//1번 노드부터 누적해서 이동한 거리를 저장
-//그 다음, 다시 거리 테이블에서 가장 작은 값을 가졌고, 방문하지 않은 노드인 2번 노드를 방문한다.
-//o번 노드를 거쳐 간 거리가 훨씬 짧기 때문에 거리 테이블에서는 값을 갱신하지 않는다.
-//위와 같은 과정을 반복(방문하지 않은 노드가 없을 때까지)
+// //작동 이해하기
+// //노드 간의 거리는 Infinity로 저장 => 무한대의 값을 저장해서 영원히 닿을 수 없다는 의미
+// //출발 노드는 1번 노드
+// //1번 노드는 자기 스스로와 거리가 0 이기 때문에 먼저 0 으로 채워넣는다.
+// //1번 노드 방문했으니 visited=true로 설정
+// //다음에 방문할 노드 찾기(거리 값이 작은 걸로 선택)
+// //1번 노드부터 누적해서 이동한 거리를 저장
+// //그 다음, 다시 거리 테이블에서 가장 작은 값을 가졌고, 방문하지 않은 노드인 2번 노드를 방문한다.
+// //o번 노드를 거쳐 간 거리가 훨씬 짧기 때문에 거리 테이블에서는 값을 갱신하지 않는다.
+// //위와 같은 과정을 반복(방문하지 않은 노드가 없을 때까지)
 
-//구현하기
-//다익스트라 알고리즘은 bfs로 기반
-//구현 방법
-//첫번째. 거리 테이블을 매번 탐색하는 ‘순차 탐색’ 방식
-//두번째. 거리가 짧은 노드를 우선 탐색하는 ‘우선순위 큐’방식
+// //구현하기
+// //다익스트라 알고리즘은 bfs로 기반
+// //구현 방법
+// //첫번째. 거리 테이블을 매번 탐색하는 ‘순차 탐색’ 방식
+// //두번째. 거리가 짧은 노드를 우선 탐색하는 ‘우선순위 큐’방식
 
-//순차탐색 방식
-// 노드 간의 거리를 초기화
-const graph = [
-  [Infinity, 1, Infinity, 2, Infinity],
-  [1, Infinity, 3, Infinity, 2],
-  [Infinity, 3, Infinity, Infinity, 1],
-  [2, Infinity, Infinity, Infinity, 2],
-  [Infinity, 2, 1, 2, Infinity],
-];
+// //순차탐색 방식
+// // 노드 간의 거리를 초기화
+// const graph = [
+//   [Infinity, 1, Infinity, 2, Infinity],
+//   [1, Infinity, 3, Infinity, 2],
+//   [Infinity, 3, Infinity, Infinity, 1],
+//   [2, Infinity, Infinity, Infinity, 2],
+//   [Infinity, 2, 1, 2, Infinity],
+// ];
 
-// 방문 여부를 기록할 배열 생성
-const visited = Array(N).fill(false);
+// // 방문 여부를 기록할 배열 생성
+// const visited = Array(N).fill(false);
 
-// 1번 노드로부터 각 노드까지의 최단 거리를 저장한 배열 생성
-const dist = visited.map((_, i) => graph[0][i]);
-//ex)[Infinity, 1, Infinity, 2, Infinity] 이런 형태
+// // 1번 노드로부터 각 노드까지의 최단 거리를 저장한 배열 생성
+// const dist = visited.map((_, i) => graph[0][i]);
+// //ex)[Infinity, 1, Infinity, 2, Infinity] 이런 형태
 
-// 방문하지 않았으면서 거리 테이블에서 가장 작은 값을 가진 노드 탐색
-const findSmallestNode = (visited, distance) => {
-  //최소 거리 초기화
-  let minDist = Infinity;
-  //최소 노드 초기화
-  let minIdx = 0;
+// // 방문하지 않았으면서 거리 테이블에서 가장 작은 값을 가진 노드 탐색
+// const findSmallestNode = (visited, distance) => {
+//   //최소 거리 초기화
+//   let minDist = Infinity;
+//   //최소 노드 초기화
+//   let minIdx = 0;
 
-  //거리값까지 순회
-  for (let i = 0; i < distance.length; i++) {
-    //방문했다면 패스
-    if (visited[i]) continue;
+//   //거리값까지 순회
+//   for (let i = 0; i < distance.length; i++) {
+//     //방문했다면 패스
+//     if (visited[i]) continue;
 
-    //현재 거리값이 최소 거리값보다 작다면
-    if (distance[i] < minDist) {
-      //거리값 갱신
-      minDist = distance[i];
-      //노드번호 갱신
-      minIdx = i;
-    }
-  }
+//     //현재 거리값이 최소 거리값보다 작다면
+//     if (distance[i] < minDist) {
+//       //거리값 갱신
+//       minDist = distance[i];
+//       //노드번호 갱신
+//       minIdx = i;
+//     }
+//   }
 
-  //가장 작은 노드 번호 반환
-  return minIdx;
-};
+//   //가장 작은 노드 번호 반환
+//   return minIdx;
+// };
 
-// 다익스트라 알고리즘 수행
-const dijkstra = (graph, visited, dist) => {
-  // 1번 노드는 거리를 0으로 설정하고, 방문한 것으로 처리
-  distance[0] = 0;
-  visited[0] = true;
+// // 다익스트라 알고리즘 수행
+// const dijkstra = (graph, visited, dist) => {
+//   // 1번 노드는 거리를 0으로 설정하고, 방문한 것으로 처리
+//   distance[0] = 0;
+//   visited[0] = true;
 
-  /*
-			현재 방문한 노드는 거리 테이블 상에서 가장 거리가 짧은 값을 가진 노드. 
-			다음에 방문할 노드에 저장된 값이
-			"현재 방문한 노드까지 누적 이동 거리 + 다음 노드까지 거리"보다 크다면
-			"현재 방문한 노드까지 누적 이동 거리 + 다음 노드까지 거리"를 거리 테이블의 다음 방문할 노드에 저장
-		*/
-  for (let i = 0; i < distance.length; i++) {
-    // 방문하지 않았으면서 거리 테이블에서 가장 작은 값을 가진 노드 탐색
-    const nodeIdx = findSmallestNode(visited, distance);
-    //방문 체크
-    visited[nodeIdx] = true;
+//   /*
+// 			현재 방문한 노드는 거리 테이블 상에서 가장 거리가 짧은 값을 가진 노드. 
+// 			다음에 방문할 노드에 저장된 값이
+// 			"현재 방문한 노드까지 누적 이동 거리 + 다음 노드까지 거리"보다 크다면
+// 			"현재 방문한 노드까지 누적 이동 거리 + 다음 노드까지 거리"를 거리 테이블의 다음 방문할 노드에 저장
+// 		*/
+//   for (let i = 0; i < distance.length; i++) {
+//     // 방문하지 않았으면서 거리 테이블에서 가장 작은 값을 가진 노드 탐색
+//     const nodeIdx = findSmallestNode(visited, distance);
+//     //방문 체크
+//     visited[nodeIdx] = true;
 
-    for (let j = 0; j < distance.length; j++) {
-      if (visited[j]) continue;
-      //현재 거리값이 "현재 방문한 노드까지 누적 이동 거리 + 다음 노드까지 거리"보다 크다면
-      if (distance[j] > distance[nodeIdx] + graph[nodeIdx][j])
-        //갱신
-        distance[j] = distance[nodeIdx] + graph[nodeIdx][j];
-    }
-  }
-};
+//     for (let j = 0; j < distance.length; j++) {
+//       if (visited[j]) continue;
+//       //현재 거리값이 "현재 방문한 노드까지 누적 이동 거리 + 다음 노드까지 거리"보다 크다면
+//       if (distance[j] > distance[nodeIdx] + graph[nodeIdx][j])
+//         //갱신
+//         distance[j] = distance[nodeIdx] + graph[nodeIdx][j];
+//     }
+//   }
+// };
 
-// 순차 탐색 방식은 ‘방문하지 않은 노드 중 거리값이 가장 작은 노드’를 다음 탐색 노드로 선택한다.
-// 그 노드를 찾는 방식은 거리 테이블의 앞에서부터 찾아내야 하기 때문에 
-// 시간 복잡도는 O(N2)이 된다.
+// // 순차 탐색 방식은 ‘방문하지 않은 노드 중 거리값이 가장 작은 노드’를 다음 탐색 노드로 선택한다.
+// // 그 노드를 찾는 방식은 거리 테이블의 앞에서부터 찾아내야 하기 때문에 
+// // 시간 복잡도는 O(N2)이 된다.
 
-//우선순위 큐
-//그래프는 2차원 배열 안에 객체 형태로 to 목적지와 dist 거리를 저장
-// 0번 노드는 사용하지 않는 빈 노드이다.
-// 이는 시작 노드를 1번으로 설정하기 위함이다.
-const graph = [
-  [], // 사용X
-  [
-    { to: 2, dist: 1 },
-    { to: 4, dist: 2 },
-  ],
-  [
-    { to: 1, dist: 1 },
-    { to: 3, dist: 3 },
-    { to: 5, dist: 2 },
-  ],
-  [
-    { to: 2, dist: 3 },
-    { to: 5, dist: 1 },
-  ],
-  [
-    { to: 1, dist: 2 },
-    { to: 5, dist: 2 },
-  ],
-  [
-    { to: 2, dist: 2 },
-    { to: 3, dist: 1 },
-    { to: 4, dist: 2 },
-  ],
-];
+// //우선순위 큐
+// //그래프는 2차원 배열 안에 객체 형태로 to 목적지와 dist 거리를 저장
+// // 0번 노드는 사용하지 않는 빈 노드이다.
+// // 이는 시작 노드를 1번으로 설정하기 위함이다.
+// const graph = [
+//   [], // 사용X
+//   [
+//     { to: 2, dist: 1 },
+//     { to: 4, dist: 2 },
+//   ],
+//   [
+//     { to: 1, dist: 1 },
+//     { to: 3, dist: 3 },
+//     { to: 5, dist: 2 },
+//   ],
+//   [
+//     { to: 2, dist: 3 },
+//     { to: 5, dist: 1 },
+//   ],
+//   [
+//     { to: 1, dist: 2 },
+//     { to: 5, dist: 2 },
+//   ],
+//   [
+//     { to: 2, dist: 2 },
+//     { to: 3, dist: 1 },
+//     { to: 4, dist: 2 },
+//   ],
+// ];
 
-// 1번 노드와 각 노드까지 최단 경로를 저장하는 배열 생성
-const dist = Array(graph.length).fill(Infinity);
+// // 1번 노드와 각 노드까지 최단 경로를 저장하는 배열 생성
+// const dist = Array(graph.length).fill(Infinity);
 
-// 큐 생성 및 1번 노드에 대한 정보 저장
-const queue = [{ to: 1, dist: 0 }];
+// // 큐 생성 및 1번 노드에 대한 정보 저장
+// const queue = [{ to: 1, dist: 0 }];
 
-// 1번 노드의 거리는 0 으로 설정
-dist[1] = 0;
+// // 1번 노드의 거리는 0 으로 설정
+// dist[1] = 0;
 
-// 큐가 빌 때까지 반복
-while (queue.length) {
-  // 큐에서 방문할 노드 꺼내기
-  const { to } = queue.pop();
+// // 큐가 빌 때까지 반복
+// while (queue.length) {
+//   // 큐에서 방문할 노드 꺼내기
+//   const { to } = queue.pop();
 
-  // 방문한 노드까지 이동한 거리 + 다음 방문 노드까지 거리를
-  // 기존에 저장된 값과 비교해서 갱신
-  graph[to].forEach((next) => {
-    //acc는 새로운 거리(방문한 노드까지 이동한 거리 + 다음 방문 노드까지 거리)
-    const acc = dist[to] + next.dist;
-    //새로운 거리가 기존 거리보다 짧다면
-    if (dist[next.to] > acc) {
-      //갱신
-      dist[next.to] = acc;
-      // 최단 경로가 되는 노드는 큐에 추가
-      queue.push(next);
-    }
-  });
-}
+//   // 방문한 노드까지 이동한 거리 + 다음 방문 노드까지 거리를
+//   // 기존에 저장된 값과 비교해서 갱신
+//   graph[to].forEach((next) => {
+//     //acc는 새로운 거리(방문한 노드까지 이동한 거리 + 다음 방문 노드까지 거리)
+//     const acc = dist[to] + next.dist;
+//     //새로운 거리가 기존 거리보다 짧다면
+//     if (dist[next.to] > acc) {
+//       //갱신
+//       dist[next.to] = acc;
+//       // 최단 경로가 되는 노드는 큐에 추가
+//       queue.push(next);
+//     }
+//   });
+// }
 
-//연습문제
-//프로그래머스-배달
-function solution(N, road, K) {
-  let graph = Array.from(Array(N + 1), () => Array());
-  let distance = Array.from({ length: N + 1 }, () => Infinity);
-  let queue = [];
+// //연습문제
+// //프로그래머스-배달
+// function solution(N, road, K) {
+//   let graph = Array.from(Array(N + 1), () => Array());
+//   let distance = Array.from({ length: N + 1 }, () => Infinity);
+//   let queue = [];
 
-  for (let [a, b, c] of road) {
-    graph[a].push([b, c]);
-    graph[b].push([a, c]);
-  }
+//   for (let [a, b, c] of road) {
+//     graph[a].push([b, c]);5
+//   }
  
-  queue.push([1, 0]);
-  distance[1] = 0;
-  while (queue.length) {
-    const [point, cost] = queue.shift();
-    for (let i = 0; i < graph[point].length; i++) {
-      const next = graph[point][i][0];
-      const nextcost = graph[point][i][1];
+//   queue.push([1, 0]);
+//   distance[1] = 0;
+//   while (queue.length) {
+//     const [point, cost] = queue.shift();
+//     for (let i = 0; i < graph[point].length; i++) {
+//       const next = graph[point][i][0];
+//       const nextcost = graph[point][i][1];
 
-      if (distance[next] > distance[point] + nextcost) {
-        distance[next] = distance[point] + nextcost;
-        queue.push([next, nextcost]);
-      }
-    }
-  }
+//       if (distance[next] > distance[point] + nextcost) {
+//         distance[next] = distance[point] + nextcost;
+//         queue.push([next, nextcost]);
+//       }
+//     }
+//   }
 
-  distance = distance.filter((v) => v <= K);
-  return distance.length;
+//   distance = distance.filter((v) => v <= K);
+//   return distance.length;
+// }
+
+//1916-최소비용
+//순차탐색 구현
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [N, M, ...input] = require('fs').readFileSync(filePath).toString().trim().split("\n");
+
+N = +N;  // 도시의 개수
+M = +M;  // 버스의 개수
+
+// 출발 도시 start, 도착 도시 end
+let [start, end] = input.pop().split(" ").map(Number);
+
+// 버스의 정보
+input = input.map((v) => v.split(" ").map(Number));
+
+// 버스의 정보를 저장할 배열
+let graph = Array.from({ length: N + 1 }, () => []);
+
+// 출발도시 a의 도착 도시 b, 비용 c를 객체 형태로 저장
+for (let [a, b, c] of input) {
+  graph[a].push({ node: b, cost: c });
 }
+
+// 최단거리를 저장할 배열
+let dist = Array.from({ length: N + 1 }).fill(Infinity);
+// 방문여부를 저장할 배열
+let visited = Array.from({ length: N + 1 }).fill(false);
+
+// 순차 탐색을 하는 경우 노드의 개수 만큼 순차 탐색을 수행해야 하기 때문에 O(N²)의 시간이 걸린다.
