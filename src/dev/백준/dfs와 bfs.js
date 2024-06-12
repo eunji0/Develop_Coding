3eea8fa6116e2b69033a1a154f04d4f40fa23c99
unknown <pih9134@naver.com>
2024-06-13 00:29:41 +0900

    최단경로//2485-가로수

--- src/dev/백준/dfs와 bfs.js
+++ src/dev/백준/dfs와 bfs.js
@@ -678,43 +678,79 @@
 // console.log(sol(input));
 
 //4485-녹색 옷 입은 애가 젤다지?
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
-const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-let N = +input.shift();
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-let answerCount = 1;
-let answer = '';
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+// const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+// let N = +input.shift();
+// const dx = [-1, 1, 0, 0];
+// const dy = [0, 0, -1, 1];
+// let answerCount = 1;
+// let answer = '';
 
-while (N !== 0) {
-    const map = input.splice(0, N).map((e) => e.split(' ').map(Number));
-    dijkstra([0, 0], map);
-    N = +input.shift();
-}
+// while (N !== 0) {
+//     const map = input.splice(0, N).map((e) => e.split(' ').map(Number));
+//     dijkstra([0, 0], map);
+//     N = +input.shift();
+// }
 
-console.log(answer.trimEnd());
+// console.log(answer.trimEnd());
 
-function dijkstra(start, map) {
-    const [x, y] = start;
-    const distance = Array.from({ length: N }, () => Array(N).fill(Infinity));
+// function dijkstra(start, map) {
+//     const [x, y] = start;
+//     const distance = Array.from({ length: N }, () => Array(N).fill(Infinity));
 
-    // 다익스트라 + BFS
-    const queue = [[x, y]];
-    let front = 0;
-    distance[y][x] = map[y][x];
+//     // 다익스트라 + BFS
+//     const queue = [[x, y]];
+//     let front = 0;
+//     distance[y][x] = map[y][x];
 
-    while (queue.length > front) {
-        const [x, y] = queue[front++];
-        for (let i = 0; i < 4; i++) {
-            const nx = x + dx[i];
-            const ny = y + dy[i];
-            if (ny >= 0 && nx >= 0 && ny < N && nx < N && distance[ny][nx] > distance[y][x] + map[ny][nx]) {
-                distance[ny][nx] = distance[y][x] + map[ny][nx];
-                queue.push([nx, ny]);
-            }
-        }
+//     while (queue.length > front) {
+//         const [x, y] = queue[front++];
+//         for (let i = 0; i < 4; i++) {
+//             const nx = x + dx[i];
+//             const ny = y + dy[i];
+//             if (ny >= 0 && nx >= 0 && ny < N && nx < N && distance[ny][nx] > distance[y][x] + map[ny][nx]) {
+//                 distance[ny][nx] = distance[y][x] + map[ny][nx];
+//                 queue.push([nx, ny]);
+//             }
+//         }
+//     }
+//     answer += `Problem ${answerCount++}: ${distance[N - 1][N - 1]}\n`;
+// }
+
+//2485-가로수
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+input.shift();
+let arr = input.map(el=>+Number(el))
+let answer = [];
+  for(let i = 0; i<arr.length-1; i++){
+    answer.push(Math.abs(arr[i]-arr[i+1]))
+  }
+
+  function gcd(a, b) { //최대공약수 구하기
+    if (a < b) {
+      [a, b] = [b, a];
     }
-    answer += `Problem ${answerCount++}: ${distance[N - 1][N - 1]}\n`;
-}
+  
+    if (b === 0) {
+      return a;
+    } else {
+      return gcd(b, a % b);
+    }
+  }
 
+  function findGCD(arr) { //여러개 숫자의 최대공약수
+    
+    let result = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      result = gcd(result, arr[i]);
+    }
+    return result;
+  }
+  
+  let num = findGCD(answer)
+  let sum = 0;
+  for(let x of answer){
+    sum += Math.floor(x/num)-1;
+  } 
+  console.log(sum)
 
