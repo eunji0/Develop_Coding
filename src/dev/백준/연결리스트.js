a0fcc5b6ba7a29762ff56b5f3f440eadcf5f310b
unknown <pih9134@naver.com>
2024-06-21 23:49:07 +0900

    연결리스트///1406-에디터

--- src/dev/백준/연결리스트.js
+++ src/dev/백준/연결리스트.js
@@ -546,3 +546,25 @@ class LinkedList {
         }
     }
 }
+
+//1406-에디터
+const [S, N, ...input] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let l_stack = [...S]; // 처음 시작할 때 커서가 맨 뒤에 있으므로, 문자열을 왼쪽 스택에 넣어준다.
+let r_stack = [];
+
+for (let i = 0; i < N; i++) {
+  const [command, value] = input[i].split(" ");
+  if (command === "L" && l_stack.length) r_stack.push(l_stack.pop());
+  else if (command === "D" && r_stack.length) l_stack.push(r_stack.pop());
+  else if (command === "B" && l_stack.length) l_stack.pop();
+  else if (command === "P") l_stack.push(value);
+}
+
+// 두 개의 스택을 합치기 전, 오른쪽 스택의 순서가 뒤집혀 있으므로 reverse()를 이용해 순서를 되돌려놓는다.
+const answer = l_stack.concat(r_stack.reverse()).join("");
+console.log(answer);
