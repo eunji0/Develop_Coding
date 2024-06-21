84dfd1b6dd1396465b1d4ec03d526cb4200339c4
unknown <pih9134@naver.com>
2024-06-22 00:41:32 +0900

    연결리스트///5397-키로거

--- src/dev/백준/연결리스트.js
+++ src/dev/백준/연결리스트.js
@@ -568,3 +568,37 @@ for (let i = 0; i < N; i++) {
 // 두 개의 스택을 합치기 전, 오른쪽 스택의 순서가 뒤집혀 있으므로 reverse()를 이용해 순서를 되돌려놓는다.
 const answer = l_stack.concat(r_stack.reverse()).join("");
 console.log(answer);
+
+//5397-키로거
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const T = input.shift();
+for (let i = 0; i<T; i++) {
+    password(input[i].split(''))
+}
+
+
+function password(str) {
+    let answer = [];
+    let idx = 0;
+    str.forEach((kp) => {
+        // console.log('현재 입력', kp);
+        // console.log('현재 커서위치', idx);
+        if (kp == '-') {
+            answer.splice(idx-1,1);
+        }
+        else if (kp == '>') {
+            idx == answer.length || idx ++;
+        }
+        else if (kp == '<') {
+            idx == 0 || idx --;
+        }
+        else {
+            answer.splice(idx, 0, kp)
+            idx ++;
+            // console.log(answer);
+        }
+    })
+    console.log(answer.join(''));
+}
