bb6e98b38eb426f28e39dc490ee4692a710ac941
unknown <pih9134@naver.com>
2024-06-23 00:28:06 +0900

    연결리스트//append 메서드 정의

--- src/dev/백준/연결리스트.js
+++ src/dev/백준/연결리스트.js
@@ -385,220 +385,198 @@
 // 특정 인덱스: O(n)
 
 //연결리스트 클래스
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.size = 0;
+// class LinkedList {
+//     constructor() {
+//         this.head = null;
+//         this.size = 0;
+//     }
+
+//     // 리스트의 끝에 노드 추가
+//     append(data) {
+
+//         //먼저, data를 저장하는 새로운 노드를 생성한다.
+//         // new Node(data)를 호출하여 Node 클래스의 인스턴스를 만든다.
+//         // 새 노드의 next 포인터는 null로 초기화된다.
+//         const newNode = new Node(data);
+
+//         //헤드 노드가 없는 경우 (리스트가 비어있는 경우)
+//         if (this.head === null) {
+
+//             //리스트가 비어있다면 새 노드가 첫 번째 노드가 된다.
+//             this.head = newNode;
+
+//             //헤드 노드가 있는 경우 (리스트가 비어있지 않은 경우)
+//         } else {
+
+//             //current 변수를 this.head로 초기화한다.
+//             let current = this.head;
+
+//             //current.next가 null이 될 때까지 리스트를 순회
+//             //이 루프는 리스트의 끝을 찾기 위한 것이다.
+//             //끝을 찾아야 맨 마지막에 추가하기 떄문.
+//             //current.next가 null이 되는 시점은 현재 노드가 리스트의 마지막 노드인 경우이다.
+//             while (current.next !== null) {
+
+//                 //루프가 종료되면 current는 리스트의 마지막 노드를 가리키고 있다.
+//                 current = current.next;
+//             }
+
+//             // 마지막 노드의 next 포인터를 새 노드로 설정한다.
+//             // 이로써 새 노드가 리스트의 끝에 추가된다.
+//             current.next = newNode;
+//         }
+
+//         //노드가 추가되었으니까
+//         //리스트의 크기를 업데이트
+//         this.size++;
+//     }
+
+//     // 특정 위치에 노드 삽입
+//     insertAt(data, index) {
+//         //유효한 인덱스인지 확인
+//         if (index < 0 || index > this.size) {
+//             return console.log("Invalid index");
+//         }
+
+//         //새로운 노드 생성
+//         //new Node(data)를 호출하여 Node 클래스의 인스턴스를 만든다.
+//         const newNode = new Node(data);
+
+//         //인덱스가 0인경우
+//         //리스트의 시작 부분에 삽입
+//         if (index === 0) {
+//             //새 노드의 next 포인터를 현재의 헤드 노드(this.head)로 설정한다.
+//             newNode.next = this.head;
+//             //그런 다음, this.head를 새 노드로 업데이트한다.
+//             this.head = newNode;
+
+
+//             //인덱스가 0이 아닌 경우
+//             //리스트의 중간 또는 끝 부분에 삽입
+//         } else {
+
+//             //인덱스가 0이 아닌 경우, 삽입하려는 위치까지 리스트를 순회한다.
+
+//             let current = this.head;  // 현재 노드를 헤드 노드로 설정
+//             let previous;             // 이전 노드를 추적하기 위한 변수
+//             let count = 0;            // 현재 노드의 인덱스를 추적하기 위한 변수
+            
+
+//             //count가 index보다 작을 때까지 루프를 반복한다
+//             while (count < index) {
+//                 previous = current;
+//                 current = current.next;
+//                 //count를 1씩 증가시켜 현재 인덱스를 추적한다.
+//                 count++;
+//             }
+//             //루프가 종료되면 
+//             //current는 삽입하려는 위치의 노드를 가리키고, 
+//             //previous는 그 이전 노드를 가리킨다.
+
+//             //새 노드의 next 포인터를 current로 설정한다.
+//             newNode.next = current;
+
+//             //previous.next를 새 노드로 설정하여 새로운 노드를 리스트에 삽입한다.
+//             previous.next = newNode;
+//         }
+
+//         //리스트의 크기를 업데이트
+//         this.size++;
+//     }
+
+//     // 노드 삭제
+//     removeAt(index) {
+//         if (index < 0 || index >= this.size) {
+//             return console.log("Invalid index");
+//         }
+
+//         let current = this.head;
+//         let previous;
+//         let count = 0;
+
+//         if (index === 0) {
+//             this.head = current.next;
+//         } else {
+//             //while문이 종료되면 current는 제거하려는 위치의 노드를 가리키고, previous는 그 이전 노드를 가리킨다.
+//             while (count < index) {
+//                 previous = current;
+//                 current = current.next;
+//                 count++;
+//             }
+
+//             //previous.next를 current.next로 설정하여 현재 노드를 리스트에서 제거한다.
+//             previous.next = current.next;
+//         }
+//         this.size--;
+
+//         //제거된 노드의 데이터를 반환한다.
+//         return current.data;
+//     }
+
+//     // 특정 위치의 노드 데이터 반환
+//     getAt(index) {
+//         if (index < 0 || index >= this.size) {
+//             return console.log("Invalid index");
+//         }
+
+//         //current 변수를 현재 헤드 노드(this.head)로 초기화한다
+//         let current = this.head;
+
+//         //count 변수는 현재 노드의 인덱스를 추적한다.
+//         let count = 0;
+
+//         while (count < index) {
+//             // current를 current.next로 설정하여 다음 노드로 이동한다.
+//             current = current.next;
+
+//             //count를 1씩 증가시켜 현재 인덱스를 추적한다.
+//             count++;
+//         }
+
+//         //현재 노드(current)의 데이터를 반환한다.
+//         return current.data;
+//     }
+
+//     // 전체 리스트 출력
+//     printList() {
+//         let current = this.head;
+//         while (current) {
+//             console.log(current.data);
+//             current = current.next;
+//         }
+//     }
+// }
+
+class Node{
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head=null;
+        this.size=0;
     }
 
-    // 리스트의 끝에 노드 추가
-    append(data) {
-
-        //먼저, data를 저장하는 새로운 노드를 생성한다.
-        // new Node(data)를 호출하여 Node 클래스의 인스턴스를 만든다.
-        // 새 노드의 next 포인터는 null로 초기화된다.
+    append(data){
         const newNode = new Node(data);
 
-        //헤드 노드가 없는 경우 (리스트가 비어있는 경우)
-        if (this.head === null) {
-
-            //리스트가 비어있다면 새 노드가 첫 번째 노드가 된다.
+        if(this.head === null){
             this.head = newNode;
-
-            //헤드 노드가 있는 경우 (리스트가 비어있지 않은 경우)
-        } else {
-
-            //current 변수를 this.head로 초기화한다.
+        }else{
             let current = this.head;
 
-            //current.next가 null이 될 때까지 리스트를 순회
-            //이 루프는 리스트의 끝을 찾기 위한 것이다.
-            //끝을 찾아야 맨 마지막에 추가하기 떄문.
-            //current.next가 null이 되는 시점은 현재 노드가 리스트의 마지막 노드인 경우이다.
-            while (current.next !== null) {
-
-                //루프가 종료되면 current는 리스트의 마지막 노드를 가리키고 있다.
+            while(current.next !== null){
                 current = current.next;
             }
 
-            // 마지막 노드의 next 포인터를 새 노드로 설정한다.
-            // 이로써 새 노드가 리스트의 끝에 추가된다.
             current.next = newNode;
         }
 
-        //노드가 추가되었으니까
-        //리스트의 크기를 업데이트
         this.size++;
     }
 
-    // 특정 위치에 노드 삽입
-    insertAt(data, index) {
-        //유효한 인덱스인지 확인
-        if (index < 0 || index > this.size) {
-            return console.log("Invalid index");
-        }
 
-        //새로운 노드 생성
-        //new Node(data)를 호출하여 Node 클래스의 인스턴스를 만든다.
-        const newNode = new Node(data);
-
-        //인덱스가 0인경우
-        //리스트의 시작 부분에 삽입
-        if (index === 0) {
-            //새 노드의 next 포인터를 현재의 헤드 노드(this.head)로 설정한다.
-            newNode.next = this.head;
-            //그런 다음, this.head를 새 노드로 업데이트한다.
-            this.head = newNode;
-
-
-            //인덱스가 0이 아닌 경우
-            //리스트의 중간 또는 끝 부분에 삽입
-        } else {
-
-            //인덱스가 0이 아닌 경우, 삽입하려는 위치까지 리스트를 순회한다.
-
-            let current = this.head;  // 현재 노드를 헤드 노드로 설정
-            let previous;             // 이전 노드를 추적하기 위한 변수
-            let count = 0;            // 현재 노드의 인덱스를 추적하기 위한 변수
-            
-
-            //count가 index보다 작을 때까지 루프를 반복한다
-            while (count < index) {
-                previous = current;
-                current = current.next;
-                //count를 1씩 증가시켜 현재 인덱스를 추적한다.
-                count++;
-            }
-            //루프가 종료되면 
-            //current는 삽입하려는 위치의 노드를 가리키고, 
-            //previous는 그 이전 노드를 가리킨다.
-
-            //새 노드의 next 포인터를 current로 설정한다.
-            newNode.next = current;
-
-            //previous.next를 새 노드로 설정하여 새로운 노드를 리스트에 삽입한다.
-            previous.next = newNode;
-        }
-
-        //리스트의 크기를 업데이트
-        this.size++;
-    }
-
-    // 노드 삭제
-    removeAt(index) {
-        if (index < 0 || index >= this.size) {
-            return console.log("Invalid index");
-        }
-
-        let current = this.head;
-        let previous;
-        let count = 0;
-
-        if (index === 0) {
-            this.head = current.next;
-        } else {
-            //while문이 종료되면 current는 제거하려는 위치의 노드를 가리키고, previous는 그 이전 노드를 가리킨다.
-            while (count < index) {
-                previous = current;
-                current = current.next;
-                count++;
-            }
-
-            //previous.next를 current.next로 설정하여 현재 노드를 리스트에서 제거한다.
-            previous.next = current.next;
-        }
-        this.size--;
-
-        //제거된 노드의 데이터를 반환한다.
-        return current.data;
-    }
-
-    // 특정 위치의 노드 데이터 반환
-    getAt(index) {
-        if (index < 0 || index >= this.size) {
-            return console.log("Invalid index");
-        }
-
-        //current 변수를 현재 헤드 노드(this.head)로 초기화한다
-        let current = this.head;
-
-        //count 변수는 현재 노드의 인덱스를 추적한다.
-        let count = 0;
-
-        while (count < index) {
-            // current를 current.next로 설정하여 다음 노드로 이동한다.
-            current = current.next;
-
-            //count를 1씩 증가시켜 현재 인덱스를 추적한다.
-            count++;
-        }
-
-        //현재 노드(current)의 데이터를 반환한다.
-        return current.data;
-    }
-
-    // 전체 리스트 출력
-    printList() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
-        }
-    }
-}
-
-//1406-에디터
-const [S, N, ...input] = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
-
-let l_stack = [...S]; // 처음 시작할 때 커서가 맨 뒤에 있으므로, 문자열을 왼쪽 스택에 넣어준다.
-let r_stack = [];
-
-for (let i = 0; i < N; i++) {
-  const [command, value] = input[i].split(" ");
-  if (command === "L" && l_stack.length) r_stack.push(l_stack.pop());
-  else if (command === "D" && r_stack.length) l_stack.push(r_stack.pop());
-  else if (command === "B" && l_stack.length) l_stack.pop();
-  else if (command === "P") l_stack.push(value);
-}
-
-// 두 개의 스택을 합치기 전, 오른쪽 스택의 순서가 뒤집혀 있으므로 reverse()를 이용해 순서를 되돌려놓는다.
-const answer = l_stack.concat(r_stack.reverse()).join("");
-console.log(answer);
-
-//5397-키로거
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const T = input.shift();
-for (let i = 0; i<T; i++) {
-    password(input[i].split(''))
-}
-
-
-function password(str) {
-    let answer = [];
-    let idx = 0;
-    str.forEach((kp) => {
-        // console.log('현재 입력', kp);
-        // console.log('현재 커서위치', idx);
-        if (kp == '-') {
-            answer.splice(idx-1,1);
-        }
-        else if (kp == '>') {
-            idx == answer.length || idx ++;
-        }
-        else if (kp == '<') {
-            idx == 0 || idx --;
-        }
-        else {
-            answer.splice(idx, 0, kp)
-            idx ++;
-            // console.log(answer);
-        }
-    })
-    console.log(answer.join(''));
 }
