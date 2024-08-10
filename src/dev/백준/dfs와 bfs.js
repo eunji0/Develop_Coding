8e25b5716f0c44c6e9fffb5a59dc42197094ceb8
unknown <pih9134@naver.com>
2024-08-11 02:59:40 +0900

    dfsbfs//4963-섬의 개수

--- src/dev/백준/dfs와 bfs.js
+++ src/dev/백준/dfs와 bfs.js
@@ -2968,51 +2968,93 @@
 
 
 //2468-안전 영역
+// const fs = require('fs');
+// const input = fs.readFileSync(process.platform === "linux" ? "dev/stdin" : "input.txt").toString().trim().split('\n');
+// const n = +input[0];
+// const arr= input.slice(1).map(v=>v.split(' ').map(Number));
+// const dir = [[0,1], [0,-1], [1,0], [-1,0]]
+
+// //개수 세기
+// const bfs = (state, i, j)=>{
+//   const queue = [[i, j]];
+
+//   while(queue.length){
+//     const [x, y] = queue.shift();
+
+//     for(const [dx, dy] of dir){
+//       const nx = x+dx;
+//       const ny = y+dy;
+
+//       if(nx>=0&&ny>=0&&nx<n&&ny<n&&state[nx][ny]===true){
+//         //방문여부 대신 false로 체크
+//         state[nx][ny]=false
+//         queue.push([nx, ny])
+//       }
+//     }
+//   }
+// }
+
+// let maxSafeArea = 0;
+
+// //h보다 낮은 지역의 상태 표시
+// for(let h=0; h<=Math.max(...arr.flat()); h++){
+//   let state = arr.map(row => row.map(value => value > h));
+
+//   let count =0;
+
+//   for(let i=0; i<n; i++){
+//     for(let j=0; j<n; j++){
+//       if(state[i][j]===true){
+//         bfs(state, i, j);
+//         count++;
+//       }
+//     }
+//   }
+  
+//   maxSafeArea=Math.max(maxSafeArea, count)
+// }
+
+// console.log(maxSafeArea)
+
+//4963-섬의 개수
 const fs = require('fs');
 const input = fs.readFileSync(process.platform === "linux" ? "dev/stdin" : "input.txt").toString().trim().split('\n');
-const n = +input[0];
-const arr= input.slice(1).map(v=>v.split(' ').map(Number));
-const dir = [[0,1], [0,-1], [1,0], [-1,0]]
+const dir = [[0,1], [0,-1], [1,0], [-1,0], [1,-1], [-1,1], [-1,-1], [1,1]]
+let arr, w, h;
 
-//개수 세기
-const bfs = (state, i, j)=>{
+const bfs = (i, j) =>{
   const queue = [[i, j]];
 
   while(queue.length){
-    const [x, y] = queue.shift();
+    const [x, y] =queue.shift();
 
     for(const [dx, dy] of dir){
       const nx = x+dx;
       const ny = y+dy;
 
-      if(nx>=0&&ny>=0&&nx<n&&ny<n&&state[nx][ny]===true){
-        //방문여부 대신 false로 체크
-        state[nx][ny]=false
-        queue.push([nx, ny])
+      if(nx>=0&&ny>=0&&nx<h&&ny<w&&arr[nx][ny]===1){
+        queue.push([nx, ny]);
+        arr[nx][ny]=true;
       }
     }
   }
 }
 
-let maxSafeArea = 0;
-
-//h보다 낮은 지역의 상태 표시
-for(let h=0; h<=Math.max(...arr.flat()); h++){
-  let state = arr.map(row => row.map(value => value > h));
+for(let i=0; i<input.length-1; i++){
+  [w, h] = input[i].split(' ').map(Number);
+  arr = input.slice(i+1, i+h+1).map(v=>v.split(' ').map(Number));
 
   let count =0;
 
-  for(let i=0; i<n; i++){
-    for(let j=0; j<n; j++){
-      if(state[i][j]===true){
-        bfs(state, i, j);
+  for(let i=0; i<h; i++){
+    for(let j=0; j<w; j++){
+      if(arr[i][j]===1){
+        bfs(i, j)
         count++;
       }
     }
   }
-  
-  maxSafeArea=Math.max(maxSafeArea, count)
+
+  console.log(count)
+  i+=h
 }
-
-console.log(maxSafeArea)
-
