0e0042765d04402c87b36cf30d09f7e04a0d319b
unknown <pih9134@naver.com>
2024-08-27 22:11:32 +0900

    dfsbfs//16928-뱀과 사다리 게임

--- src/dev/백준/dfs와 bfs.js
+++ src/dev/백준/dfs와 bfs.js
@@ -4219,33 +4219,46 @@
 //   }
 // }
 
-const solve = (input) => {
-  input.shift();
-  const temp = [...new Array(101)].map((_, i) => i);
-  for (const i of input) {
-    const [ start, end ] = i.split(' ').map(v => +v);
-    temp[start] = end;
-  }
-  const graph = [...new Array(101)]
-    .map((_, i) => [...new Array(6)]
-      .map((_, ii) => temp[i + ii + 1])
-      .filter(v => v <= 100)
-    );
-  console.log(BFS(1, graph));
-};
 
-const BFS = (start, graph) => {
-  const visited = new Array(101).fill(false);
-  const queue = [[start, 0]];
-  while (queue.length) {
-    const [ node, count ] = queue.shift();
-    if (node === 100) return count;
-    if (!visited[node]) {
-      visited[node] = true;
-      queue.push(...graph[node].map(v => [v, count + 1]));
-    }
-  }
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './Javascript/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const ladder = input.slice(1, N + 1).map((e) => e.split(' ').map(Number));
+const snake = input.slice(N + 1).map((e) => e.split(' ').map(Number));
+const visited = Array(101).fill(false);
+const arr = Array(101).fill(0);
+
+// 사다리
+for (let [x, y] of ladder) {
+    arr[x] = y;
+}
+// 뱀
+for (let [u, v] of snake) {
+    arr[u] = v;
 }
 
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-solve(input);
+let answer = bfs(1, 0);
+console.log(answer);
+
+function bfs(start, count) {
+    const queue = [[start, count]];
+    let front = 0;
+    visited[start] = true;
+    while (queue.length > front) {
+        const [v, diceCount] = queue[front++];
+        for (let i = 1; i <= 6; i++) {
+            let next = v + i;
+            if (next === 100) return diceCount + 1;
+            else if (next < 100) {
+                // 사다리 혹은 뱀일 경우 해당 위치로 이동 (카운트는 증가하지 않음)
+                if (arr[next] !== 0) {
+                    next = arr[next];
+                }
+                if (!visited[next]) {
+                    queue.push([next, diceCount + 1]);
+                    visited[next] = true;
+                }
+            }
+        }
+    }
+}
