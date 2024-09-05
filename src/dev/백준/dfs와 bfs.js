3455a47ec33f40c691903b52c4672b3f62be11b1
unknown <pih9134@naver.com>
2024-09-05 17:03:25 +0900

    dfsbfs//1325-효율적인 해킹

--- src/dev/백준/dfs와 bfs.js
+++ src/dev/백준/dfs와 bfs.js
@@ -4613,62 +4613,114 @@
 //bfs(출발지점, 목표지점)으로 거리 구하기 
 
 
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
-const [n, m] = input.shift().split(' ').map(Number)
-const arr = input.map(v=>v.split(' ').map(Number));
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs.readFileSync(filePath).toString().trim().split('\n');
+// const [n, m] = input.shift().split(' ').map(Number)
+// const arr = input.map(v=>v.split(' ').map(Number));
 
-const dir = [[0,1], [0,-1], [1,0], [-1,0]];
+// const dir = [[0,1], [0,-1], [1,0], [-1,0]];
 
-let goal = [];
-for(let i=0; i<n; i++){
-  for(let j=0; j<m; j++){
-    if(arr[i][j]===2){
-      goal=[i, j]
-      break;
-    }
-  }
+// let goal = [];
+// for(let i=0; i<n; i++){
+//   for(let j=0; j<m; j++){
+//     if(arr[i][j]===2){
+//       goal=[i, j]
+//       break;
+//     }
+//   }
 
-  if(goal){
-    break;
-  }
-}
+//   if(goal){
+//     break;
+//   }
+// }
 
-const bfs = (start)=>{
-  let distance = Array.from({length: n}, ()=>Array(m).fill(-1));
-  const [startx, starty] =start;
-  const queue=[[startx, starty]];
-  distance[startx][starty]=0;
+// const bfs = (start)=>{
+//   let distance = Array.from({length: n}, ()=>Array(m).fill(-1));
+//   const [startx, starty] =start;
+//   const queue=[[startx, starty]];
+//   distance[startx][starty]=0;
 
-  while(queue.length){
-    const [x, y] =queue.shift();
+//   while(queue.length){
+//     const [x, y] =queue.shift();
 
-    for(const [dx, dy] of dir){
-      const nx =x+dx;
-      const ny =y+dy;
+//     for(const [dx, dy] of dir){
+//       const nx =x+dx;
+//       const ny =y+dy;
       
-      if(nx >= 0 && nx < n && ny >= 0 && ny < m&&arr[nx][ny]!=0&&distance[nx][ny]===-1){
-        distance[nx][ny]=distance[x][y]+1;
-        queue.push([nx, ny])
-      }
+//       if(nx >= 0 && nx < n && ny >= 0 && ny < m&&arr[nx][ny]!=0&&distance[nx][ny]===-1){
+//         distance[nx][ny]=distance[x][y]+1;
+//         queue.push([nx, ny])
+//       }
+//     }
+//   }
+
+//   return distance
+// }
+
+// const distances = bfs(goal);
+
+// for(let i=0; i<n; i++){
+//   let r = [];
+//   for(let j=0; j<m; j++){
+//     if(arr[i][j]===0){
+//       r.push(0)
+//     }else{
+//       r.push(distances[i][j])
+//     }
+//   }
+
+//   console.log(r.join(' '))
+// }
+
+
+//1325-효율적인 해킹
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const [input, ...arr] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [N, M] = input.split(" ").map(Number);
+
+// 그래프를 만듦.
+const graph = Array.from({ length: N + 1 }, () => []);
+
+// 각 노드에 대한 간선 정보를 저장
+for (let i = 0; i < M; i++) {
+  const [front, back] = arr[i].split(" ");
+  graph[+back].push(+front);
+}
+
+// 각 노드에 대한 DFS
+function dfs(start) {
+  const stack = [start];
+  const visited = new Array(N + 1).fill(false);
+  let count = 0; // 해킹 가능한 컴퓨터 수
+  visited[start] = true; // 시작 노드 방문 처리
+
+  while (stack.length) {
+    const node = stack.pop();
+    // 해당 노드와 연결된 노드들을 탐색
+    for (let next of graph[node]) {
+      if (visited[next]) continue;
+      stack.push(next);
+      visited[next] = true;
+      count++;
     }
   }
 
-  return distance
+  return count;
 }
 
-const distances = bfs(goal);
+let max = -1;
+let answer = [];
 
-for(let i=0; i<n; i++){
-  let r = [];
-  for(let j=0; j<m; j++){
-    if(arr[i][j]===0){
-      r.push(0)
-    }else{
-      r.push(distances[i][j])
-    }
+for (let i = 1; i <= N; i++) {
+  let count = dfs(i);
+  if (count > max) {
+    max = count;
+    answer = [i];
+  } else if (count === max) {
+    answer.push(i);
   }
-
-  console.log(r.join(' '))
 }
+
+console.log(answer.join(" "));
