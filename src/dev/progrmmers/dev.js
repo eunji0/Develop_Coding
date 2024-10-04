6d8f2eb1620d95ea1c90f3ab0f15aeefdfe6a38d
unknown <pih9134@naver.com>
2024-10-05 01:01:54 +0900

    프로그래머스//의상

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -21,3 +21,14 @@ function solution(phone_book) {
 
   return true
 }
+
+//의상
+function solution(clothes) {
+  let count = new Map()
+
+  clothes.forEach(v=>{
+    count.set(v[1], (count.get(v[1])||0)+1)
+  })
+
+  return [...count.values()].reduce((a, c)=>a*(c+1), 1)-1
+}
