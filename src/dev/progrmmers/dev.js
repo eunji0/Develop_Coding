4a16e9995c91ea33ff9085ec036e40ccb97f2a86
unknown <pih9134@naver.com>
2024-10-07 15:25:47 +0900

    프로그래머스//동영상 재생기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -32,3 +32,82 @@ function solution(clothes) {
 
   return [...count.values()].reduce((a, c)=>a*(c+1), 1)-1
 }
+
+//[PCCP 기출문제] 1번 / 동영상 재생기
+
+//10초전으로 이동 - prev
+//현재 위치가 10초 미만인 경우 영상의 처음 위치로 이동
+
+//10초후로 이동 - next
+//동영상의 남은 시간이 10초 미만일 경우 영상의 마지막 위치로 이동
+
+//오프닝 건너뛰기
+//현재 재생 위치가 오프닝 구간(op_start ≤ 현재 재생 위치 ≤ op_end)인 경우 자동으로 오프닝이 끝나는 위치로 이동
+
+//동영상의 길이를 나타내는 video_len
+//기능이 수행되기 직전의 재생위치를 나타내는 pos
+//오프닝 시작 시각을 나타내는 op_start
+//오프닝이 끝나는 시각을 나타내는 op_end
+//사용자의 입력을 나타내는 1차원 commands
+
+// 오프닝 구간인지 여부
+const checkOpeningTime = (x, op_start, op_end) => {
+  return x >= op_start && x <= op_end;
+};
+
+// 시간을 초로 변환하는 함수
+const transTime = (str) => {
+  const [mm, ss] = str.split(':');
+  const result = mm * 60 + ss * 1;
+  return result;
+};
+
+// 초를 "mm:ss" 형식으로 변환하는 함수
+const transStr = (time) => {
+  let mm = parseInt(time / 60);
+  let ss = time % 60;
+  mm = String(mm).padStart(2, '0');
+  ss = String(ss).padStart(2, '0');
+  return `${mm}:${ss}`;
+};
+
+// 10초 전으로 이동하는 함수
+const prev = (x) => {
+  return x - 10 < 0 ? 0 : x - 10;
+};
+
+// 10초 후로 이동하는 함수
+const next = (x, video_len) => {
+  return x + 10 > video_len ? video_len : x + 10;
+};
+
+function solution(video_len, pos, op_start, op_end, commands) {
+  // 입력된 시간들을 모두 초 단위로 변환
+  video_len = transTime(video_len);
+  pos = transTime(pos);
+  op_start = transTime(op_start);
+  op_end = transTime(op_end);
+
+  // 오프닝 구간에 있는지 체크하고 건너뛰기
+  if (checkOpeningTime(pos, op_start, op_end)) {
+    pos = op_end;
+  }
+
+  // commands 처리
+  commands.forEach((v) => {
+    if (v === 'prev') {
+      pos = prev(pos);
+    } else if (v === 'next') {
+      pos = next(pos, video_len);
+    }
+
+    // 이동 후에 오프닝 구간에 있는지 다시 체크
+    if (checkOpeningTime(pos, op_start, op_end)) {
+      pos = op_end;
+    }
+  });
+
+  // 최종 위치를 "mm:ss" 형식으로 반환
+  return transStr(pos);
+}
+
