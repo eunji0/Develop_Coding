21be5c2443af671e7d22ca51e189c0dd1026b4b3
unknown <pih9134@naver.com>
2024-10-09 22:20:22 +0900

    프로그래머스//문자열 나누기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -506,3 +506,23 @@ function solution(s) {
     return count < 0 ? count : i - count;
   });
 }
+
+//문자열 나누기
+function solution(s) {
+  let answer = 0;
+  let current;
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (count === 0) {
+      answer++;
+      current = s[i];
+      count = 1;
+    } else {
+      if (current !== s[i]) count--;
+      else count++;
+    }
+  }
+
+  return answer;
+}
