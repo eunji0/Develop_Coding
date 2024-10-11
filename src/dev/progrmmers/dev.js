eaa23b8559c57e6c18e75781b66fc22dd48f0d74
unknown <pih9134@naver.com>
2024-10-12 01:12:03 +0900

    프로그래머스//부족한 금액 계산하기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -800,3 +800,14 @@ function solution(numbers) {
 
   return result;
 }
+
+//부족한 금액 계산하기
+function solution(price, money, count) {
+  let sum = 0;
+
+  for (let i = 1; i <= count; i++) {
+    sum += price * i;
+  }
+
+  return sum - money > 0 ? sum - money : 0;
+}
