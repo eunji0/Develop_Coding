d6faa251705e5289bf351f2dce3eb81e4a5fa44d
unknown <pih9134@naver.com>
2024-10-12 01:07:28 +0900

    프로그래머스//없는 숫자 더하기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -786,3 +786,17 @@ function solution(sizes) {
 
   return a * b;
 }
+
+//없는 숫자 더하기
+function solution(numbers) {
+  numbers.sort((a, b) => a - b);
+  let result = 0;
+
+  for (let i = 0; i < 10; i++) {
+    if (!numbers.includes(i)) {
+      result += i;
+    }
+  }
+
+  return result;
+}
