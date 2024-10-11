aadc31d4cf8ae54a62cba2e10a8f7e33528cf012
unknown <pih9134@naver.com>
2024-10-11 17:37:53 +0900

    프로그래머스//햄버거 만들기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -605,3 +605,20 @@ function solution(food) {
   }
   return answer + '0' + answer.split('').reverse().join('');
 }
+
+//햄버거 만들기
+function solution(ingredient) {
+  let stack = [];
+  let count = 0;
+
+  for (let i = 0; i < ingredient.length; i++) {
+    stack.push(ingredient[i]);
+
+    if (stack.length >= 4 && stack.slice(-4).join('') === '1231') {
+      stack.splice(-4);
+      count++;
+    }
+  }
+
+  return count;
+}
