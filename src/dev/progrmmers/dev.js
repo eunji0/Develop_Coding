aa20a23256576d054ed9589e572f75f696160dfd
unknown <pih9134@naver.com>
2024-10-12 00:47:00 +0900

    프로그래머스//나머지가 1이 되는 수 찾기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -756,3 +756,12 @@ function solution(id_list, report, k) {
 
   return Object.entries(sliceC).map((v) => v[1]);
 }
+
+//나머지가 1이 되는 수 찾기
+function solution(n) {
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 1) {
+      return i;
+    }
+  }
+}
