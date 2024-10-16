aaf6e2e192c294a6c34ff5ee9cffec66ab7364f4
unknown <pih9134@naver.com>
2024-10-16 18:05:22 +0900

    프로그래머스//1차 다트 게임

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -1119,3 +1119,85 @@ function solution(participant, completion) {
     .map((v) => v[0])
     .toString();
 }
+
+//1차 다트 게임
+function singlePoint(stack) {
+  if (stack.length > 0) {
+    const point = Math.pow(stack.pop(), 1);
+    stack.push(point);
+
+    return true;
+  }
+  return false;
+}
+function doublePoint(stack) {
+  if (stack.length > 0) {
+    const point = Math.pow(stack.pop(), 2);
+    stack.push(point);
+
+    return true;
+  }
+  return false;
+}
+function triplePoint(stack) {
+  if (stack.length > 0) {
+    const point = Math.pow(stack.pop(), 3);
+    stack.push(point);
+
+    return true;
+  }
+  return false;
+}
+function star(stack) {
+  if (stack.length > 0) {
+    const point = stack.pop() * 2;
+
+    if (stack.length >= 1) {
+      const prevPoint = stack.pop() * 2;
+      stack.push(prevPoint);
+    }
+
+    stack.push(point);
+
+    return true;
+  }
+  return false;
+}
+function acha(stack) {
+  if (stack.length > 0) {
+    const point = stack.pop() * -1;
+    stack.push(point);
+    return true;
+  }
+  return false;
+}
+
+const operators = {
+  S: singlePoint,
+  D: doublePoint,
+  T: triplePoint,
+  '*': star,
+  '#': acha,
+};
+function isOperator(character) {
+  return operators[character];
+}
+function solution(dartResult) {
+  var answer = 0;
+  const stack = [];
+
+  for (let i = 0; i < dartResult.length; i++) {
+    const number = parseInt(dartResult.slice(i));
+
+    if (Number.isInteger(number)) {
+      stack.push(number);
+      i += number.toString().length - 1;
+    } else {
+      const operator = isOperator(dartResult.charAt(i));
+      operator(stack);
+    }
+  }
+  console.log(stack);
+  answer = stack.reduce((accumulator, currentValue) => accumulator + currentValue);
+  return answer;
+}
