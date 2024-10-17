797585b708e224559e8665d4ac6cc7467c7814dc
unknown <pih9134@naver.com>
2024-10-17 18:59:18 +0900

    프로그래머스//행렬의 덧셈

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -1307,3 +1307,17 @@ function solution(x, n) {
 
   return answer;
 }
+
+//행렬의 덧셈
+function solution(arr1, arr2) {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let a = [];
+    for (let j = 0; j < arr1[0].length; j++) {
+      a.push(arr1[i][j] + arr2[i][j]);
+    }
+    result.push(a);
+  }
+
+  return result;
+}
