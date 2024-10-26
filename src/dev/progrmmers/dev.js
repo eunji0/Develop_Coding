8053335e8969591963a5e4464193b5907210fe3c
unknown <pih9134@naver.com>
2024-10-26 23:37:21 +0900

    프로그래머스//광물캐기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -1582,37 +1582,27 @@ function solution(plans) {
 //구분한 광물을 result+=
 //반복
 //5번 반복했다면 pickIndex+=1, 처음부터 반복
-
 function solution(picks, minerals) {
-  var answer = 0;
-  var cutCount = Math.ceil(minerals.length / 5);
-  // 곡갱이 개수
-  let maxLen = picks.reduce((a, b) => a + b);
-  if (maxLen === 0) return 0;
-  // 최대 구할수 있는 개수 빼고 제거
-  minerals = minerals.splice(0, maxLen * 5);
-  var arr = [];
-  for (var i = 0; i < cutCount; i++) {
-    let obj = { diamond: 0, iron: 0, stone: 0 };
+  let map = {};
 
-    minerals.splice(0, 5).map((element) => {
-      obj[element]++;
-    });
-    arr.push([
-      obj.diamond + obj.iron + obj.stone,
-      obj.diamond * 5 + obj.iron + obj.stone,
-      obj.diamond * 25 + obj.iron * 5 + obj.stone,
-    ]);
+  picks.forEach((v, i) => (map[i] = v));
+
+  map = Object.entries(map);
+  console.log(map);
+
+  let picksIndex = 0;
+  let nowNum = null;
+
+  if (map[picksIndex][1] <= 0) {
+    picksIndex++;
   }
-  arr = arr.sort((a, b) => b[2] - a[2]);
-  for (var i = 0; i < picks.length; i++) {
-    var pickCount = picks[i];
-    while (pickCount--) {
-      if (arr.length === 0) {
-        return answer;
-      }
-      answer += arr.shift()[i];
-    }
-  }
-  return answer;
+
+  nowNum = map[picksIndex][0];
+  map[picksIndex][1] -= 1;
+  let sum = map.reduce((a, c) => a + c[1], 0);
+  console.log(sum);
+
+  // while(minerals.length>0&&sum>0){
+
+  // }
 }
