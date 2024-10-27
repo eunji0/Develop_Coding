e22828e231177273aa784c94eba498f46841cc2f
unknown <pih9134@naver.com>
2024-10-27 23:52:29 +0900

    프로그래머스//리코쳇 로봇

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -1582,27 +1582,60 @@ function solution(plans) {
 //구분한 광물을 result+=
 //반복
 //5번 반복했다면 pickIndex+=1, 처음부터 반복
-function solution(picks, minerals) {
-  let map = {};
 
-  picks.forEach((v, i) => (map[i] = v));
+//리코쳇 로봇
+function solution(board) {
+  let answer = -1;
 
-  map = Object.entries(map);
-  console.log(map);
+  const map = board.map((item) => item.split(''));
+  const n = map.length;
+  const m = map[0].length;
+  const visited = new Array(n).fill().map((_) => new Array(m).fill(0));
 
-  let picksIndex = 0;
-  let nowNum = null;
+  const dx = [0, 1, 0, -1];
+  const dy = [-1, 0, 1, 0];
 
-  if (map[picksIndex][1] <= 0) {
-    picksIndex++;
+  // 시작 지점 찾기
+  let sx, sy;
+  for (let x = 0; x < n; x++) {
+    for (let y = 0; y < m; y++) {
+      if (map[x][y] === 'R') {
+        sx = x;
+        sy = y;
+      }
+    }
   }
 
-  nowNum = map[picksIndex][0];
-  map[picksIndex][1] -= 1;
-  let sum = map.reduce((a, c) => a + c[1], 0);
-  console.log(sum);
+  // BFS
+  const queue = [[sx, sy, 0]];
+  visited[sx][sy] = 1;
 
-  // while(minerals.length>0&&sum>0){
+  while (queue.length) {
+    const [x, y, count] = queue.shift();
 
-  // }
+    if (map[x][y] === 'G') {
+      answer = count;
+      break;
+    }
+
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
+
+      // 미끄러지기 (한방향으로)
+      while (0 <= nx && nx < n && 0 <= ny && ny < m && map[nx][ny] !== 'D') {
+        nx += dx[i];
+        ny += dy[i];
+      }
+      nx -= dx[i];
+      ny -= dy[i];
+
+      if (visited[nx][ny] === 0) {
+        queue.push([nx, ny, count + 1]);
+        visited[nx][ny] = 1;
+      }
+    }
+  }
+
+  return answer;
 }
