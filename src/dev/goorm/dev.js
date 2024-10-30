eab3ad00ae2c316adbfdfd4eb7f6efdfbe5c0d89
unknown <pih9134@naver.com>
2024-10-30 17:46:45 +0900

    프로그래머스//인공지능 청소기

--- src/dev/goorm/dev.js
+++ src/dev/goorm/dev.js
@@ -1,31 +1,69 @@
-//숫자 제거 배열
-const readline = require("readline");
+//인공지능 청소기
 
-(async () => {
-  let rl = readline.createInterface({ input: process.stdin });
-  let a = [];
-  let b = [];
-  let s = true;
-  let result = 0;
+//purpose
+//사용자가 설정한 시간과 장소에 정확히 도착하는 기능
 
-  for await (const line of rl) {
-    if (s) {
-      a = line.split(" ");
-      s = false;
+//input
+//첫째줄에 테스트 개수
+//도착장소(x, y) 시간 n
+
+//problem
+//동일한 위치를 두번 이상 지나쳐서 청소 가능
+//초기 로봇 청소기 위치:(0,0)
+//1초에 한번씩. 상하좌우 중 하나의 방향으로 1의 거리
+//n초의 시간에 도착지점(x,y)에 위치해야함
+
+//output
+//로봇청소기가 정확한 시간에 도착할 수 있는지 여부 YES OR NO
+
+//procedure
+//거리 구하는 함수 getDistance
+//while(t--)
+//목표좌표와 이동횟수 shift()
+//distance거리가 n보다 작을 경우, (n-dis)%2===0인경우 접근 가능
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// 맨해튼 거리 계산 함수
+const calculateDistance = (r, c, targetR, targetC) => {
+  return Math.abs(targetR - r) + Math.abs(targetC - c);
+};
+
+function solution(input) {
+  let t = Number(input.shift());
+  const results = [];
+
+  while (t--) {
+    const [x, y, n] = input.shift();
+    const distance = calculateDistance(0, 0, x, y);
+
+    // 도달 가능 여부 확인
+    if (distance <= n && (n - distance) % 2 === 0) {
+      results.push('YES');
     } else {
-      b = line.split(" ");
-      break;
+      results.push('NO');
     }
   }
 
-  b.forEach((v) => {
-    if (!v.includes(a[1])) {
-      result += 1;
-    }
-  });
+  console.log(results.join('\n'));
+}
 
-  console.log(result);
+let input = [];
 
-  rl.close();
+rl.on('line', (line) => {
+  if (!line) {
+    rl.close();
+  } else {
+    input.push(line.split(' ').map(Number));
+  }
+});
+
+rl.on('close', () => {
+  solution(input);
   process.exit();
-})();
+});
