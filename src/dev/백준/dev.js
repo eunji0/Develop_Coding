888671518d33589756a38becd1b90af1050354dd
unknown <pih9134@naver.com>
2024-11-30 23:04:49 +0900

    백준//14003-가장 긴 증가하는 부분 수열 5

--- src/dev/백준/dev.js
+++ src/dev/백준/dev.js
@@ -6499,24 +6499,83 @@
 // console.log(answer);
 
 //1072-게임
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-let [x, y] = input[0].split(' ').map(Number);
-let z = Math.floor((100 * y) / x);
-let left = 1;
-let right = 1000000000;
-let ans = Infinity;
-while (left <= right) {
-  let mid = parseInt((left + right) / 2);
-  let new_z = Math.floor((100 * (y + mid)) / (x + mid));
-  if (z !== new_z) {
-    ans = Math.min(ans, mid);
-    right = mid - 1;
-  } else {
-    left = mid + 1;
+// let [x, y] = input[0].split(' ').map(Number);
+// let z = Math.floor((100 * y) / x);
+// let left = 1;
+// let right = 1000000000;
+// let ans = Infinity;
+// while (left <= right) {
+//   let mid = parseInt((left + right) / 2);
+//   let new_z = Math.floor((100 * (y + mid)) / (x + mid));
+//   if (z !== new_z) {
+//     ans = Math.min(ans, mid);
+//     right = mid - 1;
+//   } else {
+//     left = mid + 1;
+//   }
+// }
+// if (ans === Infinity) console.log(-1);
+// else console.log(ans);
+
+//14003-가장 긴 증가하는 부분 수열 5
+const fs = require('fs');
+const input = fs
+  .readFileSync('./dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((v) => v.split(' ').map(Number));
+const [N] = input[0];
+const arr = input[1];
+
+function binarySearch(left, right, target) {
+  let mid;
+  while (left < right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (lis[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
   }
+  return right;
 }
-if (ans === Infinity) console.log(-1);
-else console.log(ans);
+
+let lis = [];
+let records = [];
+records[0] = 0;
+let j = 0;
+lis[0] = arr[0];
+let i = 1;
+
+while (i < N) {
+  if (lis[j] < arr[i]) {
+    lis[++j] = arr[i];
+    records[i] = j;
+  } else {
+    let idx = binarySearch(0, j, arr[i]);
+    lis[idx] = arr[i];
+    records[i] = idx;
+  }
+  i++;
+}
+let answer = [];
+
+let max = Math.max(...records);
+const maxIdx = records.indexOf(max);
+
+for (let i = maxIdx; i >= 0; i--) {
+  if (records[i] == max) {
+    answer.push(arr[i]);
+    max--;
+  }
+  if (max < 0) break;
+}
+
+console.log(answer.length);
+console.log(answer.reverse().join(' '));
