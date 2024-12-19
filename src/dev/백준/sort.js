3817aa70bbe550ba5bfbbc8261bd680c27487e7a
unknown <pih9134@naver.com>
2024-12-20 00:29:43 +0900

    백준//2752-세수정렬

--- src/dev/백준/sort.js
+++ src/dev/백준/sort.js
@@ -1,4 +1,26 @@
 //2217-로프
+// const fs = require('fs');
+// const input = fs
+//   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+//   .toString()
+//   .trim()
+//   .split('\n');
+
+// let n = +input[0];
+
+// let arr = input.slice(1).map(Number);
+
+// arr.sort((a, b) => b - a);
+
+// let max = 0;
+
+// for (let i = 0; i < n; i++) {
+//   max = Math.max(max, arr[i] * (i + 1));
+// }
+
+// console.log(max);
+
+//2752-세수정렬
 const fs = require('fs');
 const input = fs
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
@@ -6,16 +28,5 @@ const input = fs
   .trim()
   .split('\n');
 
-const N = input.shift();
-
-const rope = input.map(Number);
-
-rope.sort((a, b) => b - a);
-
-const answer = [];
-
-for (let i = 0; i < N; i++) {
-  answer.push(rope[i] * (i + 1));
-}
-
-console.log(Math.max(...answer));
+const arr = input[0].split(' ').map(Number);
+console.log(arr.sort((a, b) => a - b).join(' '));
