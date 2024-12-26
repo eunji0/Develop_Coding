7fafa32d077cb501992906fdb2c818902dad0b05
unknown <pih9134@naver.com>
2024-12-26 17:55:53 +0900

    백준//11000-강의실배정

--- src/dev/백준/sort.js
+++ src/dev/백준/sort.js
@@ -265,6 +265,32 @@
 // console.log(result);
 
 //1940-주몽
+// const fs = require('fs');
+// const input = fs
+//   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+//   .toString()
+//   .trim()
+//   .split('\n');
+
+// let n = +input[0];
+// let m = +input[1];
+// let arr = input[2].split(' ').map(Number);
+
+// arr.sort((a, b) => a - b);
+
+// let count = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[i] + arr[j] === m) {
+//       count += 1;
+//     }
+//   }
+// }
+
+// console.log(count);
+
+//11000-강의실 배정
 const fs = require('fs');
 const input = fs
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
@@ -272,20 +298,27 @@ const input = fs
   .trim()
   .split('\n');
 
-let n = +input[0];
-let m = +input[1];
-let arr = input[2].split(' ').map(Number);
+input.shift();
 
-arr.sort((a, b) => a - b);
+let times = [];
 
-let count = 0;
+input.forEach((v) => {
+  v = v.split(' ').map(Number);
+  times.push([v[0], 1]);
+  times.push([v[1], -1]);
+});
 
-for (let i = 0; i < arr.length; i++) {
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[i] + arr[j] === m) {
-      count += 1;
-    }
-  }
+times.sort((a, b) => {
+  if (a[0] === b[0]) return a[1] - b[1];
+  return a[0] - b[0];
+});
+
+let answer = 0;
+let result = 0;
+
+for (let i = 0; i < times.length; i++) {
+  result += times[i][1];
+  answer = Math.max(answer, result);
 }
 
-console.log(count);
+console.log(answer);
