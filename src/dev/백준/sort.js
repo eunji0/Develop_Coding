ebf424d2341662f4ebe34589c700661089d77f2b
unknown <pih9134@naver.com>
2024-12-27 18:27:16 +0900

    백준//2075-n번째 큰수

--- src/dev/백준/sort.js
+++ src/dev/백준/sort.js
@@ -336,19 +336,93 @@
 // console.log([...newArr].sort((a, b) => a - b).join(' '));
 
 //2693-n번째 큰수
-const fs = require('fs');
-const input = fs
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
-  .toString()
-  .trim()
-  .split('\n');
+// const fs = require('fs');
+// const input = fs
+//   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+//   .toString()
+//   .trim()
+//   .split('\n');
 
-let n = +input[0];
+// let n = +input[0];
 
-for (let i = 1; i <= n; i++) {
-  let arr = input[i]
-    .split(' ')
-    .map(Number)
-    .sort((a, b) => b - a);
-  console.log(arr[2]);
+// for (let i = 1; i <= n; i++) {
+//   let arr = input[i]
+//     .split(' ')
+//     .map(Number)
+//     .sort((a, b) => b - a);
+//   console.log(arr[2]);
+// }
+
+//2075-n번째 큰수
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(value) {
+    let cur = this.heap.length;
+    let parent;
+    while (cur > 1) {
+      parent = Math.floor(cur / 2);
+      if (value < this.heap[parent]) {
+        this.heap[cur] = this.heap[parent];
+        cur = parent;
+      } else break;
+    }
+    this.heap[cur] = value;
+  }
+
+  pop() {
+    if (this.heap.length === 1) return null;
+    if (this.heap.length === 2) return this.heap.pop();
+    const popValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+    let cur = 1;
+    let left = cur * 2;
+    let right = cur * 2 + 1;
+    while (this.heap[left]) {
+      let childIdx = left;
+      if (this.heap[right] && this.heap[left] > this.heap[right]) childIdx = right;
+      if (this.heap[cur] > this.heap[childIdx]) {
+        [this.heap[cur], this.heap[childIdx]] = [this.heap[childIdx], this.heap[cur]];
+        cur = childIdx;
+        left = cur * 2;
+        right = cur * 2 + 1;
+      } else break;
+    }
+    return popValue;
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
 }
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let N = -1;
+let cnt;
+const que = new MinHeap();
+rl.on('line', (line) => {
+  if (N === -1) {
+    N = parseInt(line);
+    cnt = N;
+    idxCheck = new Array(N).fill(N - 1);
+    return;
+  }
+  const temp = line.split(' ').map(Number);
+  temp.forEach((num, idx) => {
+    que.push(num);
+    if (que.size() > N) que.pop();
+  });
+  cnt -= 1;
+  if (cnt === 0) {
+    rl.close();
+  }
+}).on('close', () => {
+  console.log(que.pop());
+});
