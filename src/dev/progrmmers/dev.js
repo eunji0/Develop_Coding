9c7e077f4c19f8b00b6fc65efa07915331f9eb01
unknown <pih9134@naver.com>
2025-02-13 20:46:11 +0900

    프로그래머스//모음사전

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -2299,3 +2299,8 @@ function solution(n, wires) {
 
   return min;
 }
+
+//모음사전
+function solution(words) {
+  return words.split('').reduce((r, c, i) => r + [781, 156, 31, 6, 1][i] * ['A', 'E', 'I', 'O', 'U'].indexOf(c) + 1, 0);
+}
