9e9510324666f65fe586958d8cf85624fcf31a5e
unknown <pih9134@naver.com>
2025-02-15 00:24:02 +0900

    프로그래머스//구명보트

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -2367,3 +2367,20 @@ function solution(number, k) {
   stack = stack.slice(0, stack.length - k);
   return stack.join('');
 }
+
+//구명보트
+function solution(people, limit) {
+  people.sort((a, b) => a - b);
+  let count = 0;
+  let left = 0;
+  let right = people.length - 1;
+
+  while (left <= right) {
+    if (people[left] + people[right] <= limit) {
+      left++;
+    }
+    right--;
+    count++;
+  }
+  return count;
+}
