fe86279ace9f21e7affdd4281aaad9a0ea9e042a
unknown <pih9134@naver.com>
2025-02-18 17:02:06 +0900

    프로그래머스//정수 삼각형

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -2460,3 +2460,17 @@ function solution(N, number) {
 
   return -1;
 }
+
+//정수 삼각형
+function solution(triangle) {
+  let n = triangle.length;
+  let dp = Array.from({ length: n }, (_, i) => [...triangle[i]]);
+
+  for (let i = n - 2; i >= 0; i--) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      dp[i][j] += Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+    }
+  }
+
+  return dp[0][0];
+}
