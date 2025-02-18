fc01a96aac8c46b8c1afa69b1c355f2cf12bb74b
unknown <pih9134@naver.com>
2025-02-19 01:18:33 +0900

    프로그래머스//등굣길

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -2474,3 +2474,29 @@ function solution(triangle) {
 
   return dp[0][0];
 }
+
+//등굣길
+function solution(m, n, puddles) {
+  let dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
+
+  for (let [a, b] of puddles) {
+    dp[b][a] = -1;
+  }
+
+  dp[1][1] = 1;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (i === 1 && j === 1) continue;
+      if (dp[i][j] === -1) {
+        dp[i][j] = 0;
+      } else {
+        let up = i > 1 ? dp[i - 1][j] : 0;
+        let left = j > 1 ? dp[i][j - 1] : 0;
+        dp[i][j] = (up + left) % 1000000007;
+      }
+    }
+  }
+
+  return dp[n][m];
+}
