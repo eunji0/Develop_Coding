18288696e3cc5cfc47b9148562bf11c56e8ed97d
unknown <pih9134@naver.com>
2025-02-20 00:24:40 +0900

    프로그래머스//입국심사

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -2722,3 +2722,19 @@ function solution(game_board, table) {
 
   return filledCount;
 }
+
+//입국심사
+function solution(n, times) {
+  let low = 1;
+  let high = Math.max(...times) * n;
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const people = times.reduce((acc, time) => acc + Math.floor(mid / time), 0);
+    if (people < n) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return low;
+}
