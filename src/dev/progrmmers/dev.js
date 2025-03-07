bdfa30bb5013b9e0bfa98670718bcc79cd252982
unknown <pih9134@naver.com>
2025-03-08 00:14:03 +0900

    프로그래머스//귤 고르기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -3965,3 +3965,16 @@ function solution(k, tangerine) {
 
   return kind;
 }
+
+//멀리 뛰기
+function solution(n) {
+  let dp = Array(n).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+  }
+
+  return dp[n];
+}
