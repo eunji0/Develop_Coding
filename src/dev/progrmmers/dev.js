ae0fcb61592032e60f7e3f3667d14fd90819d096
unknown <pih9134@naver.com>
2025-03-10 20:33:41 +0900

    프로그래머스//예상 대진표

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -4017,3 +4017,16 @@ function solution(n, words) {
 
   return [0, 0];
 }
+
+//에상 대진표
+function solution(n, a, b) {
+  let count = 0;
+
+  while (a !== b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    count++;
+  }
+
+  return count;
+}
