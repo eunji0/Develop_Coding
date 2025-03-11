a59bd5e5e2e45e9f0d961f020696359b6cfaa83f
unknown <pih9134@naver.com>
2025-03-12 00:52:41 +0900

    프로그래머스//피로도

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -4203,3 +4203,25 @@ function solution(cacheSize, cities) {
 
   return result;
 }
+
+//피로도
+function solution(k, dungeons) {
+  let maxCount = 0;
+  let visited = Array(dungeons.length).fill(false);
+
+  function dfs(k, count) {
+    maxCount = Math.max(maxCount, count);
+
+    for (let i = 0; i < dungeons.length; i++) {
+      if (!visited[i] && k - dungeons[i][0] >= 0) {
+        visited[i] = true;
+        dfs(k - dungeons[i][1], count + 1);
+        visited[i] = false;
+      }
+    }
+  }
+
+  dfs(k, 0);
+
+  return maxCount;
+}
