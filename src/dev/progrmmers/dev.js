fb11eb053892331a06390c3c12e89b6760e359d9
unknown <pih9134@naver.com>
2025-03-11 19:24:25 +0900

    프로그래머스//n^2 배열 자르기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -4103,3 +4103,16 @@ function solution(want, number, discount) {
 
   return count;
 }
+
+//n^2 배열 자르기
+function solution(n, left, right) {
+  let arr = [];
+
+  for (let i = left; i <= right; i++) {
+    let row = Math.floor(i / n);
+    let col = i % n;
+    arr.push(Math.max(row, col) + 1);
+  }
+
+  return arr;
+}
