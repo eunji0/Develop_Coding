cba4c0ed5675e784875495906335aab3d0d83747
unknown <pih9134@naver.com>
2025-03-13 00:18:41 +0900

    프로그래머스//타겟 넘버

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -4269,3 +4269,20 @@ function solution(priorities, location) {
   }
   return priorities;
 }
+
+//타겟 넘버
+function solution(numbers, target) {
+  let count = 0;
+  function dfs(index, sum) {
+    if (index === numbers.length) {
+      if (sum === target) count++;
+      return;
+    }
+
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
+  }
+
+  dfs(0, 0);
+  return count;
+}
