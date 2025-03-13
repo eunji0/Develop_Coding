10629806c279f20c3369379cd3833a6b7f1dd252
unknown <pih9134@naver.com>
2025-03-13 19:34:04 +0900

    프로그래머스//롤케이크 자르기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -4286,3 +4286,29 @@ function solution(numbers, target) {
   dfs(0, 0);
   return count;
 }
+
+//롤케이크 자르기
+function solution(topping) {
+  let leftSet = new Set();
+  let rightMap = new Map();
+  let result = 0;
+
+  for (let t of topping) {
+    rightMap.set(t, (rightMap.get(t) || 0) + 1);
+  }
+
+  for (let i = 0; i < topping.length - 1; i++) {
+    leftSet.add(topping[i]);
+    rightMap.set(topping[i], rightMap.get(topping[i]) - 1);
+
+    if (rightMap.get(topping[i]) === 0) {
+      rightMap.delete(topping[i]);
+    }
+
+    if (leftSet.size === rightMap.size) {
+      result++;
+    }
+  }
+
+  return result;
+}
