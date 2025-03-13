f32676b025ee0152f17741505bed13793a546615
unknown <pih9134@naver.com>
2025-03-13 23:27:23 +0900

    프로그래머스//모음사전

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -4368,3 +4368,18 @@ function solution(dirs) {
 
   return dirSet.size / 2;
 }
+
+//모음사전
+function solution(word) {
+  const vowels = ['A', 'E', 'I', 'O', 'U'];
+  const weights = [781, 156, 31, 6, 1];
+
+  let answer = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    let idx = vowels.indexOf(word[i]);
+    answer += idx * weights[i] + 1;
+  }
+
+  return answer;
+}
