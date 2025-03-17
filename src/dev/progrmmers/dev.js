0b4d927a416e45a3fd03eec21771f81c1463d1fc
unknown <pih9134@naver.com>
2025-03-17 19:50:30 +0900

    프로그래머스//2 x n 타일링

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -4718,3 +4718,22 @@ function solution(record) {
   result.map((v) => (v[1] = map[v[0]]));
   return result.map((v) => v[1] + v[2]);
 }
+
+//2 x n 타일링
+function solution(n) {
+  const MOD = 1000000007;
+
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+
+  let dp = new Array(n + 1).fill(0);
+
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+  }
+
+  return dp[n];
+}
