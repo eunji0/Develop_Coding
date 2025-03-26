b77ed8222520b190c34b48de663aaddd0aa76f14
unknown <pih9134@naver.com>
2025-03-26 17:11:44 +0900

    프로그래머스//하노이의 탑

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -5594,3 +5594,20 @@ function solution(places) {
 
   return result;
 }
+
+//하노이의 탑
+function hanoi(n, start, end, via, answer) {
+  if (n === 1) {
+    answer.push([start, end]);
+    return;
+  }
+
+  hanoi(n - 1, start, via, end, answer);
+  answer.push([start, end]);
+  hanoi(n - 1, via, end, start, answer);
+}
+function solution(n) {
+  let answer = [];
+  hanoi(n, 1, 3, 2, answer);
+  return answer;
+}
