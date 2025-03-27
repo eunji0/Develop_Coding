bd80a4306fae7f320921ceb8f23a13cd24e2e92a
unknown <pih9134@naver.com>
2025-03-27 19:12:22 +0900

    프로그래머스//과제 진행하기

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -5778,3 +5778,49 @@ function solution(diffs, times, limit) {
 
   return answer;
 }
+
+//과제 진행하기
+function timeToMin(time) {
+  const [hh, mm] = time.split(':').map(Number);
+  return hh * 60 + mm;
+}
+
+function solution(plans) {
+  const answer = [];
+
+  const sortedPlans = plans
+    .map(([subject, time, count]) => [subject, timeToMin(time), Number(count)])
+    .sort((a, b) => a[1] - b[1]);
+
+  const stack = [];
+
+  for (let i = 0; i < sortedPlans.length - 1; i++) {
+    const [subject, time, count] = sortedPlans[i];
+
+    if (time + count <= sortedPlans[i + 1][1]) {
+      answer.push(subject);
+      let availableTime = sortedPlans[i + 1][1] - time - count;
+
+      while (stack.length) {
+        const [currentSubject, currentTime] = stack.pop();
+        if (currentTime <= availableTime) {
+          availableTime -= currentTime;
+          answer.push(currentSubject);
+        } else {
+          stack.push([currentSubject, currentTime - availableTime]);
+          break;
+        }
+      }
+    } else {
+      stack.push([subject, count - (sortedPlans[i + 1][1] - time)]);
+    }
+  }
+
+  answer.push(sortedPlans[sortedPlans.length - 1][0]);
+
+  while (stack.length) {
+    answer.push(stack.pop()[0]);
+  }
+
+  return answer;
+}
