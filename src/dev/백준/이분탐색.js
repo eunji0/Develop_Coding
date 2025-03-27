ee5709d7164fa4435113d6f0ef61045756aeeab0
unknown <pih9134@naver.com>
2025-03-28 00:01:25 +0900

    백준//10816-숫자카드2

--- src/dev/백준/이분탐색.js
+++ src/dev/백준/이분탐색.js
@@ -1,4 +1,26 @@
 //1920-수찾기
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let n = input[0];
+// let nArr = new Set(input[1].split(' ').map(Number));
+// let m = input[2];
+// let mArr = input[3].split(' ').map(Number);
+
+// mArr.forEach((v) => {
+//   if (nArr.has(v)) {
+//     console.log(1);
+//   } else {
+//     console.log(0);
+//   }
+// });
+
+//10816-숫자카드2
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs
@@ -7,15 +29,20 @@ const input = fs
   .split('\n')
   .map((v) => v.trim());
 
-let n = input[0];
-let nArr = new Set(input[1].split(' ').map(Number));
-let m = input[2];
+let n = +input[0];
+let nArr = input[1].split(' ').map(Number);
+let m = +input[2];
 let mArr = input[3].split(' ').map(Number);
 
-mArr.forEach((v) => {
-  if (nArr.has(v)) {
-    console.log(1);
-  } else {
-    console.log(0);
-  }
+let nMap = new Map();
+
+nArr.forEach((v) => {
+  nMap.set(v, (nMap.get(v) || 0) + 1);
 });
+
+let result = [];
+mArr.forEach((v) => {
+  result.push(nMap.get(v) || 0);
+});
+
+console.log(result.join(' '));
