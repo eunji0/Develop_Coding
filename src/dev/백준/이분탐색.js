53546da4a334e6eb7ae03839ac476471b95eabb4
unknown <pih9134@naver.com>
2025-03-28 18:51:28 +0900

    백준//2110-공유기 설치

--- src/dev/백준/이분탐색.js
+++ src/dev/백준/이분탐색.js
@@ -115,40 +115,70 @@
 // console.log(result);
 
 //10815-숫자카드
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let n = +input[0];
+// let nArr = input[1]
+//   .split(' ')
+//   .map(Number)
+//   .sort((a, b) => a - b);
+// let m = +input[2];
+// let mArr = input[3].split(' ').map(Number);
+
+// const checkArr = (target) => {
+//   let left = 0;
+//   let right = nArr.length - 1;
+
+//   while (left <= right) {
+//     let mid = Math.floor((left + right) / 2);
+
+//     if (nArr[mid] === target) {
+//       return 1;
+//     } else if (nArr[mid] < target) {
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+//   return 0;
+// };
+
+// let result = mArr.map((v) => checkArr(v));
+
+// console.log(result.join(' '));
+
+//2110-공유기 설치
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const input = fs
-  .readFileSync(filePath)
-  .toString()
-  .split('\n')
-  .map((v) => v.trim());
-
-let n = +input[0];
-let nArr = input[1]
-  .split(' ')
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const [n, m] = input[0].split(' ').map(Number);
+const arr = input
+  .slice(1)
   .map(Number)
   .sort((a, b) => a - b);
-let m = +input[2];
-let mArr = input[3].split(' ').map(Number);
 
-const checkArr = (target) => {
-  let left = 0;
-  let right = nArr.length - 1;
+let start = 1;
+let end = arr[arr.length - 1];
 
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
 
-    if (nArr[mid] === target) {
-      return 1;
-    } else if (nArr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+  let count = 1;
+  let prev = arr[0];
+  for (let cur of arr) {
+    if (cur - prev < mid) continue;
+    prev = cur;
+    count += 1;
   }
-  return 0;
-};
 
-let result = mArr.map((v) => checkArr(v));
+  if (count < m) end = mid - 1;
+  else start = mid + 1;
+}
 
-console.log(result.join(' '));
+console.log(end);
