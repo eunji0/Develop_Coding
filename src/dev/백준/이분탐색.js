99b05ee105823803973d261805316f196db161fa
unknown <pih9134@naver.com>
2025-03-28 18:22:37 +0900

    백준//10815-숫자카드

--- src/dev/백준/이분탐색.js
+++ src/dev/백준/이분탐색.js
@@ -85,6 +85,36 @@
 // console.log(bestH);
 
 //1654-랜선자르기
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let [k, n] = input[0].split(' ').map(Number);
+// let arr = input.slice(1).map(Number);
+
+// let min = 1;
+// let max = Math.max(...arr);
+// let result = 0;
+
+// while (min <= max) {
+//   let mid = Math.floor((min + max) / 2);
+//   let sum = arr.reduce((a, c) => a + Math.floor(c / mid), 0);
+
+//   if (sum >= n) {
+//     result = mid;
+//     min = mid + 1;
+//   } else {
+//     max = mid - 1;
+//   }
+// }
+
+// console.log(result);
+
+//10815-숫자카드
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs
@@ -93,23 +123,32 @@ const input = fs
   .split('\n')
   .map((v) => v.trim());
 
-let [k, n] = input[0].split(' ').map(Number);
-let arr = input.slice(1).map(Number);
+let n = +input[0];
+let nArr = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+let m = +input[2];
+let mArr = input[3].split(' ').map(Number);
 
-let min = 1;
-let max = Math.max(...arr);
-let result = 0;
+const checkArr = (target) => {
+  let left = 0;
+  let right = nArr.length - 1;
 
-while (min <= max) {
-  let mid = Math.floor((min + max) / 2);
-  let sum = arr.reduce((a, c) => a + Math.floor(c / mid), 0);
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
 
-  if (sum >= n) {
-    result = mid;
-    min = mid + 1;
-  } else {
-    max = mid - 1;
+    if (nArr[mid] === target) {
+      return 1;
+    } else if (nArr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
-}
+  return 0;
+};
 
-console.log(result);
+let result = mArr.map((v) => checkArr(v));
+
+console.log(result.join(' '));
