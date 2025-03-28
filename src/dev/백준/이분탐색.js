ca936ec884e9c8862f22252b3df11c04eed32166
unknown <pih9134@naver.com>
2025-03-28 17:49:34 +0900

    백준//1654-랜선자르기

--- src/dev/백준/이분탐색.js
+++ src/dev/백준/이분탐색.js
@@ -48,6 +48,43 @@
 // console.log(result.join(' '));
 
 //2805-나무 자르기
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let [n, m] = input[0].split(' ').map(Number);
+// let arr = input[1].split(' ').map(Number);
+
+// let min = 0;
+// let max = Math.max(...arr);
+// let bestH = 0;
+// let mid = 0;
+
+// while (min <= max) {
+//   mid = Math.floor((min + max) / 2);
+//   let sum = 0;
+
+//   arr.map((v) => {
+//     if (v - mid > 0) {
+//       sum += v - mid;
+//     }
+//   });
+
+//   if (sum >= m) {
+//     if (mid > bestH) bestH = mid;
+//     min = mid + 1;
+//   } else {
+//     max = mid - 1;
+//   }
+// }
+
+// console.log(bestH);
+
+//1654-랜선자르기
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs
@@ -56,30 +93,23 @@ const input = fs
   .split('\n')
   .map((v) => v.trim());
 
-let [n, m] = input[0].split(' ').map(Number);
-let arr = input[1].split(' ').map(Number);
+let [k, n] = input[0].split(' ').map(Number);
+let arr = input.slice(1).map(Number);
 
-let min = 0;
+let min = 1;
 let max = Math.max(...arr);
-let bestH = 0;
-let mid = 0;
+let result = 0;
 
 while (min <= max) {
-  mid = Math.floor((min + max) / 2);
-  let sum = 0;
+  let mid = Math.floor((min + max) / 2);
+  let sum = arr.reduce((a, c) => a + Math.floor(c / mid), 0);
 
-  arr.map((v) => {
-    if (v - mid > 0) {
-      sum += v - mid;
-    }
-  });
-
-  if (sum >= m) {
-    if (mid > bestH) bestH = mid;
+  if (sum >= n) {
+    result = mid;
     min = mid + 1;
   } else {
     max = mid - 1;
   }
 }
 
-console.log(bestH);
+console.log(result);
