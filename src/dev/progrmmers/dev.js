5815b4824927a75df5403b5370553a7cdc621cfb
unknown <pih9134@naver.com>
2025-04-04 00:45:05 +0900

    프로그래머스//지게차와 크레인

--- src/dev/progrmmers/dev.js
+++ src/dev/progrmmers/dev.js
@@ -5863,3 +5863,204 @@ function solution(schedules, timelogs, startday) {
 //5 6 7 1 2 3 4
 //6과 7은 주말이므로 제외
 //순차적으로 확인
+
+//지게차와 크레인
+
+2;
+3;
+4;
+5;
+6;
+7;
+8;
+9;
+10;
+11;
+12;
+13;
+14;
+15;
+16;
+17;
+18;
+19;
+20;
+21;
+22;
+23;
+24;
+25;
+26;
+27;
+28;
+29;
+30;
+31;
+32;
+33;
+34;
+35;
+36;
+37;
+38;
+39;
+40;
+41;
+42;
+43;
+44;
+45;
+46;
+47;
+48;
+49;
+50;
+51;
+52;
+53;
+54;
+55;
+56;
+57;
+58;
+59;
+60;
+61;
+62;
+63;
+64;
+65;
+66;
+67;
+68;
+69;
+70;
+71;
+72;
+73;
+74;
+75;
+76;
+77;
+78;
+79;
+80;
+81;
+82;
+83;
+84;
+85;
+86;
+87;
+88;
+89;
+90;
+91;
+92;
+93;
+94;
+95;
+96;
+97;
+function solution(storage, requests) {
+  const maxRow = storage.length - 1,
+    maxColumn = storage[0].length - 1;
+
+  // 남은 컨테이너 수
+  let containerCnt = storage.length * storage[0].length;
+
+  // 컨테이너 위치와 값을 가지는 2차원 배열 생성
+  let storageMap = storage.map((string) => {
+    return string.split('');
+  });
+  // storageMap[row][column] = true 일 경우 => 외부에서 접근 가능한 빈 공간
+  // storageMap[row][column] = false 일 경우 => 외부에서 접근 불가능 한 빈공간
+
+  // r,c 가 storageMap 내부에 있는 row와 column인지
+  let isInside = (r, c) => {
+    if (r >= 0 && r <= maxRow && c >= 0 && c <= maxColumn) {
+      return true;
+    }
+    return false;
+  };
+
+  // row,column이 외부에서 접근 가능한지
+  const isOutside = (row, column) => {
+    // 동서남북
+    let dr = [0, 0, 1, -1],
+      dc = [1, -1, 0, 0];
+    for (let i = 0; i < 4; ++i) {
+      let [nearR, nearC] = [row + dr[i], column + dc[i]];
+      if (nearR < 0 || nearR > maxRow || nearC < 0 || nearC > maxColumn) {
+        return true;
+      } else if (
+        nearR >= 0 &&
+        nearR <= maxRow &&
+        nearC >= 0 &&
+        nearC <= maxColumn &&
+        storageMap[nearR][nearC] === true
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const resetOutside = () => {
+    for (let r = 0; r <= maxRow; ++r) {
+      for (let c = 0; c <= maxColumn; ++c) {
+        if (isOutside(r, c) && storageMap[r][c] === false) {
+          let arr = [[r, c]];
+
+          while (arr.length) {
+            let [row, column] = arr.shift();
+            storageMap[row][column] = true;
+
+            let dr = [0, 0, 1, -1];
+            let dc = [1, -1, 0, 0];
+
+            for (let i = 0; i < 4; ++i) {
+              let [nearR, nearC] = [row + dr[i], column + dc[i]];
+
+              if (isInside(nearR, nearC) && isOutside(nearR, nearC) && storageMap[nearR][nearC] === false) {
+                arr.push([nearR, nearC]);
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+
+  requests.forEach((request) => {
+    let alphabet = request[0];
+    resetOutside();
+    if (request.length === 1) {
+      let pickContainers = [];
+
+      for (let r = 0; r <= maxRow; ++r) {
+        for (let c = 0; c <= maxColumn; ++c) {
+          if (isOutside(r, c) && alphabet === storageMap[r][c]) {
+            pickContainers.push([r, c]);
+          }
+        }
+      }
+
+      pickContainers.forEach(([r, c]) => {
+        --containerCnt;
+        storageMap[r][c] = true;
+      });
+    } else {
+      for (let r = 0; r <= maxRow; ++r) {
+        for (let c = 0; c <= maxColumn; ++c) {
+          if (storageMap[r][c] === alphabet) {
+            --containerCnt;
+            storageMap[r][c] = false;
+          }
+        }
+      }
+    }
+  });
+
+  return containerCnt;
+}
