d81f9c46484cb90194d8c350965f465ab63a6f37
unknown <pih9134@naver.com>
2025-04-06 23:23:07 +0900

    백준//1967 - 트리의 지름

--- src/dev/백준/트리.js
+++ src/dev/백준/트리.js
@@ -45,40 +45,68 @@
 // console.log(result);
 
 //1725-트리의 부모 찾기
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// let [N, ...edges] = input;
+
+// const graph = Array.from({ length: +N + 1 }).map(() => []);
+// const checked = Array.from({ length: +N + 1 }).fill(false);
+// const parentNodes = Array.from({ length: +N + 1 }).fill(null);
+
+// edges.forEach((edge) => {
+//   const [start, end] = edge.split(' ');
+
+//   graph[start].push(+end);
+//   graph[end].push(+start);
+// });
+
+// const dfsSearchForParent = (vertex) => {
+//   if (checked[vertex]) return;
+
+//   checked[vertex] = true;
+
+//   graph[vertex].forEach((child) => {
+//     if (!checked[child]) parentNodes[child] = vertex;
+
+//     dfsSearchForParent(child);
+//   });
+// };
+
+// dfsSearchForParent(1);
+
+// let answer = '';
+
+// for (let i = 2; i < parentNodes.length; i++) {
+//   answer += parentNodes[i] + '\n';
+// }
+
+// console.log(answer);
+
+//1967 - 트리의 지름
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
-let [N, ...edges] = input;
 
-const graph = Array.from({ length: +N + 1 }).map(() => []);
-const checked = Array.from({ length: +N + 1 }).fill(false);
-const parentNodes = Array.from({ length: +N + 1 }).fill(null);
-
-edges.forEach((edge) => {
-  const [start, end] = edge.split(' ');
-
-  graph[start].push(+end);
-  graph[end].push(+start);
+const N = Number(input.shift());
+const inputArr = input.map((v) => v.split(' ').map(Number));
+let graph = Array.from(Array(N + 1), () => []);
+inputArr.forEach(([from, to, distance]) => {
+  graph[from].push([to, distance]);
+  graph[to].push([from, distance]);
 });
-
-const dfsSearchForParent = (vertex) => {
-  if (checked[vertex]) return;
-
-  checked[vertex] = true;
-
-  graph[vertex].forEach((child) => {
-    if (!checked[child]) parentNodes[child] = vertex;
-
-    dfsSearchForParent(child);
-  });
+const visited = Array(N + 1).fill(false);
+let max = { node: 0, dist: 0 };
+const dfs = (node, dist) => {
+  visited[node] = true;
+  if (max.dist < dist) max = { node, dist };
+  for (const [nextNode, nextDist] of graph[node]) {
+    if (visited[nextNode]) continue;
+    dfs(nextNode, dist + nextDist);
+  }
 };
-
-dfsSearchForParent(1);
-
-let answer = '';
-
-for (let i = 2; i < parentNodes.length; i++) {
-  answer += parentNodes[i] + '\n';
-}
-
-console.log(answer);
+dfs(1, 0); // 루트 노드(1번)로부터 가장 먼 노드 찾기위해 dfs 실행
+visited.fill(false); // 방문 배열 초기화
+dfs(max.node, 0); // 가중치가 가장 큰(가장 먼) 노드에서부터 dfs 실행
+console.log(max.dist);
