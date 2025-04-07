915dcc69309f6b02d2fce26c2607dd007b4ea87d
unknown <pih9134@naver.com>
2025-04-07 23:18:08 +0900

    백준//1991-트리 순회

---
+++ src/dev/백준/트리.js
@@ -0,0 +1,45 @@
+//1991-트리 순회
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const N = Number(input.shift());
+let result = '';
+
+const tree = {};
+for (let i = 0; i < N; i++) {
+  const [node, left, right] = input[i].split(' ');
+  tree[node] = [left, right];
+}
+
+function preorder(node) {
+  if (node === '.') return;
+  const [left, right] = tree[node];
+  result += node;
+  preorder(left);
+  preorder(right);
+}
+
+function inorder(node) {
+  if (node === '.') return;
+  const [left, right] = tree[node];
+  inorder(left);
+  result += node;
+  inorder(right);
+}
+
+function postorder(node) {
+  if (node === '.') return;
+  const [left, right] = tree[node];
+  postorder(left);
+  postorder(right);
+  result += node;
+}
+
+preorder('A');
+result += '\n';
+inorder('A');
+result += '\n';
+postorder('A');
+
+console.log(result);
