0004c82467b51a4feac22bf0f5f794eafc129902
unknown <pih9134@naver.com>
2025-04-29 19:11:23 +0900

    백준//11726-2×n 타일링

--- src/dev/백준/dp.js
+++ src/dev/백준/dp.js
@@ -54,6 +54,33 @@
 // }
 
 //1003-피보나치 함수
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .trim()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let t = +input[0];
+// let dp = Array(41)
+//   .fill()
+//   .map((v) => [0, 0]);
+// dp[0] = [1, 0];
+// dp[1] = [0, 1];
+
+// for (let i = 2; i <= 40; i++) {
+//   dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+//   dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+// }
+
+// for (let i = 1; i <= t; i++) {
+//   let n = +input[i];
+//   console.log(dp[n][0], dp[n][1]);
+// }
+
+//11726-2×n 타일링
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 let input = fs
@@ -63,19 +90,15 @@ let input = fs
   .split('\n')
   .map((v) => v.trim());
 
-let t = +input[0];
-let dp = Array(41)
-  .fill()
-  .map((v) => [0, 0]);
-dp[0] = [1, 0];
-dp[1] = [0, 1];
+let n = +input[0];
+let dp = Array(1001).fill(0);
 
-for (let i = 2; i <= 40; i++) {
-  dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
-  dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+dp[0] = 1;
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 }
 
-for (let i = 1; i <= t; i++) {
-  let n = +input[i];
-  console.log(dp[n][0], dp[n][1]);
-}
+console.log(dp[n]);
