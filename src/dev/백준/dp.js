8d2c61bcf1d447e60f10bfda2f53f6c5b789aca3
unknown <pih9134@naver.com>
2025-04-30 18:25:54 +0900

    백준//11053-가장 긴 증가하는 부분 수열

--- src/dev/백준/dp.js
+++ src/dev/백준/dp.js
@@ -128,6 +128,64 @@
 // console.log(dp[n - 1]);
 
 //2775-부녀회장이 될테야
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .trim()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let t = +input[0];
+// let idx = 1;
+
+// const dp = Array.from({ length: 15 }, () => Array(15).fill(0));
+
+// for (let i = 1; i <= 14; i++) {
+//   dp[0][i] = i;
+// }
+
+// for (let k = 1; k <= 14; k++) {
+//   for (let n = 1; n <= 14; n++) {
+//     dp[k][n] = dp[k - 1][n] + dp[k][n - 1];
+//   }
+// }
+
+// while (t--) {
+//   let k = +input[idx++];
+//   let n = +input[idx++];
+
+//   console.log(dp[k][n]);
+// }
+
+//1149-RGB거리
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .trim()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let n = +input[0];
+// let prices = input.slice(1).map((v) => v.split(' ').map(Number));
+// const dp = Array.from({ length: n }, () => Array(3).fill(0));
+
+// dp[0][0] = prices[0][0];
+// dp[0][1] = prices[0][1];
+// dp[0][2] = prices[0][2];
+
+// for (let i = 1; i < n; i++) {
+//   dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + prices[i][0];
+//   dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + prices[i][1];
+//   dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + prices[i][2];
+// }
+
+// console.log(Math.min(...dp[n - 1]));
+
+//11053-가장 긴 증가하는 부분 수열
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 let input = fs
@@ -137,24 +195,17 @@ let input = fs
   .split('\n')
   .map((v) => v.trim());
 
-let t = +input[0];
-let idx = 1;
+let n = +input[0];
+let arr = input[1].split(' ').map(Number);
 
-const dp = Array.from({ length: 15 }, () => Array(15).fill(0));
+let dp = Array(n).fill(1);
 
-for (let i = 1; i <= 14; i++) {
-  dp[0][i] = i;
-}
-
-for (let k = 1; k <= 14; k++) {
-  for (let n = 1; n <= 14; n++) {
-    dp[k][n] = dp[k - 1][n] + dp[k][n - 1];
+for (let i = 1; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (arr[j] < arr[i]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
   }
 }
 
-while (t--) {
-  let k = +input[idx++];
-  let n = +input[idx++];
-
-  console.log(dp[k][n]);
-}
+console.log(Math.max(...dp));
