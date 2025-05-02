23dc8c5ba7c344d8c31fbddd081d6dcba282ab06
unknown <pih9134@naver.com>
2025-05-03 01:23:30 +0900

    백준//9465-스티커

--- src/dev/백준/dp.js
+++ src/dev/백준/dp.js
@@ -341,22 +341,52 @@
 // console.log(dp[0]);
 
 //2747-피보나치 수
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .trim()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let n = +input[0];
+// let dp = Array(45).fill(0);
+// dp[0] = 0;
+// dp[1] = 1;
+
+// for (let i = 2; i <= 45; i++) {
+//   dp[i] = dp[i - 1] + dp[i - 2];
+// }
+
+// console.log(dp[n]);
+
+//9465-스티커
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-let input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split('\n')
-  .map((v) => v.trim());
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-let n = +input[0];
-let dp = Array(45).fill(0);
-dp[0] = 0;
-dp[1] = 1;
+let T = +input[0];
+let idx = 1;
 
-for (let i = 2; i <= 45; i++) {
-  dp[i] = dp[i - 1] + dp[i - 2];
+for (let t = 0; t < T; t++) {
+  let n = +input[idx++];
+  let top = input[idx++].split(' ').map(Number);
+  let bottom = input[idx++].split(' ').map(Number);
+
+  let dp = Array.from({ length: 2 }, () => Array(n).fill(0));
+  dp[0][0] = top[0];
+  dp[1][0] = bottom[0];
+
+  if (n > 1) {
+    dp[0][1] = bottom[0] + top[1];
+    dp[1][1] = top[0] + bottom[1];
+  }
+
+  for (let i = 2; i < n; i++) {
+    dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + top[i];
+    dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + bottom[i];
+  }
+
+  console.log(Math.max(dp[0][n - 1], dp[1][n - 1]));
 }
-
-console.log(dp[n]);
