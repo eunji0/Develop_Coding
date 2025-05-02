36536cf81a55db57d412a586bebf0a47dfea9ef2
unknown <pih9134@naver.com>
2025-05-02 16:55:16 +0900

    백준//11727-2×n 타일링 2

--- src/dev/백준/dp.js
+++ src/dev/백준/dp.js
@@ -268,6 +268,31 @@
 // }
 
 //9461-파도반 수열
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .trim()
+//   .split('\n')
+//   .map((v) => v.trim());
+
+// let t = +input[0];
+// let arr = input.slice(1).map(Number);
+
+// let dp = Array(101).fill(0);
+
+// dp[1] = 1;
+// dp[2] = 1;
+// dp[3] = 1;
+
+// for (let i = 4; i < 101; i++) {
+//   dp[i] = dp[i - 2] + dp[i - 3];
+// }
+
+// arr.forEach((v) => console.log(dp[v]));
+
+//11727-2×n 타일링 2
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 let input = fs
@@ -277,17 +302,14 @@ let input = fs
   .split('\n')
   .map((v) => v.trim());
 
-let t = +input[0];
-let arr = input.slice(1).map(Number);
+let n = +input[0];
+let dp = Array(1001).fill(0);
 
-let dp = Array(101).fill(0);
-
+dp[0] = 1;
 dp[1] = 1;
-dp[2] = 1;
-dp[3] = 1;
 
-for (let i = 4; i < 101; i++) {
-  dp[i] = dp[i - 2] + dp[i - 3];
+for (let i = 2; i < 1001; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
 }
 
-arr.forEach((v) => console.log(dp[v]));
+console.log(dp[n]);
