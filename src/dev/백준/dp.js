16d4359281f6325628afaa40ff1a3a430cc8fe08
unknown <pih9134@naver.com>
2025-05-23 19:59:50 +0900

    백준//1463-1로 만들기

--- src/dev/백준/dp.js
+++ src/dev/백준/dp.js
@@ -677,23 +677,46 @@
 // console.log(result);
 
 //2839-설탕 배달
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// let n = +input[0];
+
+// let dp = Array(5001).fill(-1);
+
+// dp[3] = 1;
+// dp[5] = 1;
+
+// for (let i = 6; i <= n; i++) {
+//   if (dp[i - 3] !== -1) dp[i] = dp[i - 3] + 1;
+//   if (dp[i - 5] !== -1) {
+//     if (dp[i] === -1 || dp[i] > dp[i - 5] + 1) {
+//       dp[i] = dp[i - 5] + 1;
+//     }
+//   }
+// }
+
+// console.log(dp[n]);
+
+//1463-1로 만들기
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 let n = +input[0];
 
-let dp = Array(5001).fill(-1);
+let dp = Array(1000001).fill(0);
 
-dp[3] = 1;
-dp[5] = 1;
+for (let i = 2; i <= n; i++) {
+  dp[i] = dp[i - 1] + 1;
 
-for (let i = 6; i <= n; i++) {
-  if (dp[i - 3] !== -1) dp[i] = dp[i - 3] + 1;
-  if (dp[i - 5] !== -1) {
-    if (dp[i] === -1 || dp[i] > dp[i - 5] + 1) {
-      dp[i] = dp[i - 5] + 1;
-    }
+  if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  }
+
+  if (i % 3 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
   }
 }
 
