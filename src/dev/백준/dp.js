114847fb7d974f0f445ae60e0f11946e2c099798
unknown <pih9134@naver.com>
2025-05-24 17:46:08 +0900

    백준//2579-계단 오르기

--- src/dev/백준/dp.js
+++ src/dev/백준/dp.js
@@ -767,19 +767,39 @@
 // }
 
 //11726-2×n 타일링
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// let n = +input[0];
+
+// let dp = Array(n + 1).fill(0);
+
+// dp[1] = 1;
+// dp[2] = 2;
+
+// for (let i = 3; i <= n; i++) {
+//   dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+// }
+
+// console.log(dp[n]);
+
+//2579-계단 오르기
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 let n = +input[0];
+let arr = input.slice(1).map(Number);
 
-let dp = Array(n + 1).fill(0);
+let dp = Array(n).fill(0);
 
-dp[1] = 1;
-dp[2] = 2;
+dp[0] = arr[0]; //첫번째계단
+dp[1] = arr[0] + arr[1];
+dp[2] = Math.max(arr[0] + arr[2], arr[1] + arr[2]);
 
-for (let i = 3; i <= n; i++) {
-  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+for (let i = 3; i < n; i++) {
+  dp[i] = Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
 }
 
-console.log(dp[n]);
+console.log(dp[n - 1]);
