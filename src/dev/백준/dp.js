68d66f3c8528bb83c8308b0266a70c4d9563aebd
unknown <pih9134@naver.com>
2025-05-26 18:55:20 +0900

    백준//2748-피보나치 수2

--- src/dev/백준/dp.js
+++ src/dev/백준/dp.js
@@ -973,17 +973,35 @@
 // console.log(maxSum);
 
 //11727-2×n 타일링 2
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// let n = +input[0];
+// const dp = Array(n + 1).fill(0);
+// dp[1] = 1;
+// dp[2] = 3;
+
+// for (let i = 3; i <= n; i++) {
+//   dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+// }
+
+// console.log(dp[n]);
+
+//2748-피보나치 수2
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 let n = +input[0];
-const dp = Array(n + 1).fill(0);
-dp[1] = 1;
-dp[2] = 3;
+let MAX = 90;
+let dp = Array(MAX + 1).fill(0n);
 
-for (let i = 3; i <= n; i++) {
-  dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+dp[0] = 0n;
+dp[1] = 1n;
+
+for (let i = 2; i <= MAX; i++) {
+  dp[i] = dp[i - 1] + dp[i - 2];
 }
 
-console.log(dp[n]);
+console.log(dp[n].toString());
