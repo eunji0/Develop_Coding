5451ed134a6fd78f9f54c738caf7a9a3969bb13e
unknown <pih9134@naver.com>
2025-05-28 00:05:12 +0900

    백준//5585-거스름돈

--- src/dev/백준/그리디.js
+++ src/dev/백준/그리디.js
@@ -123,21 +123,42 @@
 // console.log(sum);
 
 //2720-세탁소 사장 동혁
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// let n = +input[0];
+// let arr = input.slice(1).map(Number);
+// const prices = [25, 10, 5, 1];
+
+// arr.forEach((v) => {
+//   let result = [0, 0, 0, 0];
+
+//   for (let i = 0; i < 4; i++) {
+//     result[i] = Math.floor(v / prices[i]);
+//     v %= prices[i];
+//   }
+
+//   console.log(result.join(' '));
+// });
+
+//5585-거스름돈
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 let n = +input[0];
-let arr = input.slice(1).map(Number);
-const prices = [25, 10, 5, 1];
+let prices = [500, 100, 50, 10, 5, 1];
 
-arr.forEach((v) => {
-  let result = [0, 0, 0, 0];
+let num = 1000 - n;
 
-  for (let i = 0; i < 4; i++) {
-    result[i] = Math.floor(v / prices[i]);
-    v %= prices[i];
+let count = 0;
+
+for (let i = 0; i < 6; i++) {
+  if (num >= prices[i]) {
+    count += Math.floor(num / prices[i]);
+    num %= prices[i];
   }
+}
 
-  console.log(result.join(' '));
-});
+console.log(count);
