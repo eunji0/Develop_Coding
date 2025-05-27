c115947cc1676d4e485aafb41753004e40f8e586
unknown <pih9134@naver.com>
2025-05-27 18:14:45 +0900

    백준//11399-ATM

--- src/dev/백준/그리디.js
+++ src/dev/백준/그리디.js
@@ -1,20 +1,38 @@
 //2839-설탕 배달
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// let n = +input[0];
+// let count = 0;
+
+// while (n >= 0) {
+//   if (n % 5 === 0) {
+//     count += Math.floor(n / 5);
+//     console.log(count);
+//     return;
+//   }
+
+//   n -= 3;
+//   count++;
+// }
+
+// console.log(-1);
+
+//11399-ATM
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 let n = +input[0];
+let arr = input[1].split(' ').map(Number);
+
+arr = arr.map((v, i) => (v = [i + 1, v]));
+
+arr.sort((a, b) => a[1] - b[1]);
+
 let count = 0;
+let result = [];
 
-while (n >= 0) {
-  if (n % 5 === 0) {
-    count += Math.floor(n / 5);
-    console.log(count);
-    return;
-  }
-
-  n -= 3;
-  count++;
-}
-
-console.log(-1);
+arr.map((v) => result.push((count += v[1])));
+console.log(result.reduce((a, c) => a + c, 0));
