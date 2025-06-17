08d6261cd32601bdaf1c9256a58b377dd668f85e
unknown <pih9134@naver.com>
2025-06-17 21:57:01 +0900

    2023 KAKAO BLIND RECRUITMENT //이모티콘 할인행사

--- src/dev/progrmmers/2023 KAKAO BLIND RECRUITMENT.js
+++ src/dev/progrmmers/2023 KAKAO BLIND RECRUITMENT.js
@@ -43,3 +43,59 @@ function solution(today, terms, privacies) {
 
   return result;
 }
+
+//이모티콘 할인행사
+
+//문제 정리
+// 이모티콘 플러스 서비스 가입자를 최대한 늘리는 것.
+// 이모티콘 판매액을 최대한 늘리는 것.
+// 1번 목표가 우선이며, 2번 목표가 그 다음
+//각 사용자들은 일정 비율 이상 할인하는 이모티콘을 모두 구매
+//이모티콘 구매 비용의 합이 일정 가격 이상이 된다면, 이모티콘 구매를 모두 취소하고 이모티콘 플러스 서비스에 가입
+
+function solution(users, emoticons) {
+  const discounts = [10, 20, 30, 40];
+  let maxJoin = 0;
+  let maxProfit = 0;
+
+  const dfs = (depth, currentCombo) => {
+    if (depth === emoticons.length) {
+      let join = 0;
+      let profit = 0;
+
+      for (let [minRate, minPrice] of users) {
+        let total = 0;
+
+        for (let i = 0; i < emoticons.length; i++) {
+          const sale = currentCombo[i];
+          if (sale >= minRate) {
+            const discounted = (emoticons[i] * (100 - sale)) / 100;
+            total += discounted;
+          }
+        }
+
+        if (total >= minPrice) {
+          join++;
+        } else {
+          profit += total;
+        }
+      }
+
+      if (join > maxJoin || (join === maxJoin && profit > maxProfit)) {
+        maxJoin = join;
+        maxProfit = profit;
+      }
+
+      return;
+    }
+
+    for (let rate of discounts) {
+      currentCombo.push(rate);
+      dfs(depth + 1, currentCombo);
+      currentCombo.pop();
+    }
+  };
+
+  dfs(0, []);
+  return [maxJoin, Math.floor(maxProfit)];
+}
